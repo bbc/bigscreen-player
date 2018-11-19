7c5a71f5353a6180cc9eadb37f0c3ac57d5d0244
magnuswoody <magnus.woodgate@bbc.co.uk>
2018-11-19 14:50:32 +0000

    Created playback spinner & toggle menu logic

--- samples/getting-started/script/index.js
+++ samples/getting-started/script/index.js
@@ -1,46 +1,87 @@
 define([
   'bigscreenplayer/bigscreenplayer',
   'bigscreenplayer/models/windowtypes',
-  'bigscreenplayer/debugger/debugtool'
+  'bigscreenplayer/debugger/debugtool',
+  'bigscreenplayer/playbackspinner'
 ],
-  function (BigscreenPlayer, WindowTypes, DebugTool) {
+  function (BigscreenPlayer, WindowTypes, DebugTool, PlaybackSpinner) {
+
     var playbackElement = document.getElementById('videoContainer');
+    var controlsElement = document.getElementById('bsp_controls');
     var playButton = document.getElementById('playButton');
     var pauseButton = document.getElementById('pauseButton');
     var seekForwardButton = document.getElementById('forwardButton');
     var seekBackButton = document.getElementById('backButton');
     var seekToEndButton = document.getElementById('seekEndButton');
-    var toggleDebugTool = document.getElementById('toggleDebugToolButton');
-    var timeLabel = document.getElementById('timeLabel');
-    var latestState = document.getElementById('latestState');
+    var debugToolButton = document.getElementById('toggleDebugToolButton');
+    var controlsTimeout;
 
+    // Create playback spinner
+    var playbackSpinner = new PlaybackSpinner();
+
+    // Set controls to invisible initially
+    controlsElement.style.display = "none";
+
+    // Toggle on screen playback controls
+    function toggleControls() {
+      controlsElement.style.display = controlsElement.style.display == "none" ? "block" : "none";
+      if(controlsElement.style.display === "block") {
+        playButton.focus();
+      }
+    }
+
+    // Timeout feature for controls
+    function startControlsTimeOut() {
+
+      clearTimeout(controlsTimeout);
+
+      if(controlsElement.style.display === "block") {
+        controlsTimeout = setTimeout(function () {
+          toggleControls();
+        }, 5000);
+      } else {
+        toggleControls();
+      }
+    }
+
+    // Create event listeners 
     function setupControls () {
+
+      window.addEventListener('keydown', function() {
+        startControlsTimeOut();
+      });
+
       playButton.addEventListener('click', function () {
         bigscreenPlayer.play();
+        startControlsTimeOut();
       });
 
       pauseButton.addEventListener('click', function () {
         bigscreenPlayer.pause();
+        startControlsTimeOut();
       });
 
       seekForwardButton.addEventListener('click', function () {
         bigscreenPlayer.setCurrentTime(bigscreenPlayer.getCurrentTime() + 10);
+        startControlsTimeOut();
       });
 
       seekBackButton.addEventListener('click', function () {
         bigscreenPlayer.setCurrentTime(bigscreenPlayer.getCurrentTime() - 10);
+        startControlsTimeOut();
       });
 
       seekToEndButton.addEventListener('click', function () {
         bigscreenPlayer.setCurrentTime(bigscreenPlayer.getDuration() - 10);
+        startControlsTimeOut();
       });
 
-      toggleDebugTool.addEventListener('click', function () {
+      debugToolButton.addEventListener('click', function () {
         DebugTool.toggleVisibility();
+        startControlsTimeOut();
       });
 
       bigscreenPlayer.registerForTimeUpdates(function (event) {
-        timeLabel.innerText = 'Current Time: ' + event.currentTime;
       });
 
       bigscreenPlayer.registerForStateChanges(function (event) {
@@ -53,14 +94,24 @@ define([
           case 5: state = 'ENDED'; break;
           case 6: state = 'FATAL_ERROR'; break;
         }
-        latestState.innerText = 'Latest State: ' + state;
+        if(state === 'WAITING') {
+          playbackElement.appendChild(playbackSpinner);
+        } else {
+          playbackElement.removeChild(playbackSpinner);
+        }
       });
     }
 
+    // Create player instance
     var bigscreenPlayer = BigscreenPlayer();
 
+    // Init UI controls
     setupControls();
 
+    // Set initial focus
+    playButton.focus();
+
+    // Create data source for bigscreen-plauer
     var minimalData = {
       media: {
         mimeType: 'video/mp4',
@@ -72,10 +123,14 @@ define([
       }
     };
 
+    // Specify extra input args
     var windowType = WindowTypes.STATIC;
     var liveSupport = 'seekable';
     var enableSubtitles = false;
 
+    // Initialise the player
+    // At this point TAL environment can be injected, if needed
     bigscreenPlayer.init(playbackElement, minimalData, windowType, enableSubtitles, liveSupport);
+
   }
 );
