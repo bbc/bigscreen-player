2eca92ce843ef0ebf7f697c2ae82e8f330e045a3
magnuswoody <magnus.woodgate@bbc.co.uk>
2019-01-21 16:00:59 +0000

    Moved first part of LiveSeekablePlayer over from TAL

---
+++ script/playbackstrategy/modifiers/live/seekable.js
@@ -0,0 +1,154 @@
+/**
+ * @fileOverview Requirejs module containing device modifier for live playback
+ * with support level Seekable
+ * @preserve Copyright (c) 2013-present British Broadcasting Corporation. All rights reserved.
+ * @license See https://github.com/bbc/tal/blob/master/LICENSE for full licence
+ */
+
+define(
+    'bigscreenplayer/playbackstrategy/modifiers/live/seekable',
+    [
+        'bigscreenplayer/playbackstrategy/modifiers/html5',
+        'bigscreenplayer/playbackstrategy/modifiers/mediaplayerbase'
+    ],
+    function (Html5Player, MediaPlayerBase) {
+        'use strict';
+        var AUTO_RESUME_WINDOW_START_CUSHION_SECONDS = 8;
+
+        /**
+         * Live player for devices that have full support for playing and seeking live streams.
+         * Implements all functions of the underlying {antie.devices.mediaplayer.MediaPlayerBase}.
+         * See the documentation for that class for API details.
+         * @name antie.devices.mediaplayer.live.Seekable
+         * @class
+         * @extends antie.Class
+         */
+
+        var mediaPlayer; 
+
+        function SeekableLivePlayer () {
+
+            return ({
+                init: function init () {
+                    mediaPlayer = new Html5Player();
+                },
+
+                initialiseMedia: function initialiseMedia (mediaType, sourceUrl, mimeType, sourceContainer, opts) {
+                    if (mediaType === MediaPlayerBase.TYPE.AUDIO) {
+                        mediaType = MediaPlayerBase.TYPE.LIVE_AUDIO;
+                    } else {
+                        mediaType = MediaPlayerBase.TYPE.LIVE_VIDEO;
+                    }
+
+                    mediaPlayer.initialiseMedia(mediaType, sourceUrl, mimeType, sourceContainer, opts);
+                },
+
+                beginPlayback: function beginPlayback () {
+                    var config = RuntimeContext.getDevice().getConfig();
+                    if (config && config.streaming && config.streaming.overrides && config.streaming.overrides.forceBeginPlaybackToEndOfWindow) {
+                        mediaPlayer.beginPlaybackFrom(Infinity);
+                    } else {
+                        mediaPlayer.beginPlayback();
+                    }
+                },
+
+                beginPlaybackFrom: function beginPlaybackFrom (offset) {
+                    mediaPlayer.beginPlaybackFrom(offset);
+                },
+
+                playFrom: function playFrom (offset) {
+                    mediaPlayer.playFrom(offset);
+                },
+
+                pause: function pause (opts) {
+                    opts = opts || {};
+                    var secondsUntilStartOfWindow = mediaPlayer.getCurrentTime() - mediaPlayer.getSeekableRange().start;
+
+                    if (opts.disableAutoResume) {
+                        mediaPlayer.pause();
+                    } else if (secondsUntilStartOfWindow <= AUTO_RESUME_WINDOW_START_CUSHION_SECONDS) {
+                        // IPLAYERTVV1-4166
+                        // We can't pause so close to the start of the sliding window, so do a quick state transition in and
+                        // out on 'pause' state to be consistent with the rest of TAL.
+                        mediaPlayer._toPaused();
+                        mediaPlayer._toPlaying();
+                    } else {
+                        mediaPlayer.pause();
+                        _autoResumeAtStartOfRange();
+                    }
+                },
+                resume: function resume () {
+                    mediaPlayer.resume();
+                },
+
+                stop: function stop () {
+                    mediaPlayer.stop();
+                },
+
+                reset: function reset () {
+                    mediaPlayer.reset();
+                },
+
+                getState: function getState () {
+                    return mediaPlayer.getState();
+                },
+
+                getSource: function getSource () {
+                    return mediaPlayer.getSource();
+                },
+
+                getCurrentTime: function getCurrentTime () {
+                    return mediaPlayer.getCurrentTime();
+                },
+
+                getSeekableRange: function getSeekableRange () {
+                    return mediaPlayer.getSeekableRange();
+                },
+
+                getMimeType: function getMimeType () {
+                    return mediaPlayer.getMimeType();
+                },
+
+                addEventCallback: function addEventCallback (thisArg, callback) {
+                    mediaPlayer.addEventCallback(thisArg, callback);
+                },
+
+                removeEventCallback: function removeEventCallback (thisArg, callback) {
+                    mediaPlayer.removeEventCallback(thisArg, callback);
+                },
+
+                removeAllEventCallbacks: function removeAllEventCallbacks () {
+                    mediaPlayer.removeAllEventCallbacks();
+                },
+
+                getPlayerElement: function getPlayerElement () {
+                    return mediaPlayer.getPlayerElement();
+                },
+
+                getLiveSupport: function getLiveSupport () {
+                    return MediaPlayerBase.LIVE_SUPPORT.SEEKABLE;
+                },
+
+                _autoResumeAtStartOfRange: function _autoResumeAtStartOfRange () {
+                    var self = this;
+                    var secondsUntilAutoResume = Math.max(0, mediaPlayer.getCurrentTime() - mediaPlayer.getSeekableRange().start - AUTO_RESUME_WINDOW_START_CUSHION_SECONDS);
+
+                    var autoResumeTimer = setTimeout(function () {
+                        self.removeEventCallback(self, detectIfUnpaused);
+                        self.resume();
+                    }, secondsUntilAutoResume * 1000);
+
+                    this.addEventCallback(this, detectIfUnpaused);
+                    function detectIfUnpaused(event) {
+                        if (event.state !== MediaPlayerBase.STATE.PAUSED) {
+                            self.removeEventCallback(self, detectIfUnpaused);
+                            clearTimeout(autoResumeTimer);
+                        }
+                    }
+                }
+        });
+    }
+
+    return SeekableLivePlayer();
+
+});
--- script/playbackstrategy/nativehtml5strategy.js
+++ script/playbackstrategy/nativehtml5strategy.js
@@ -2,17 +2,18 @@ define('bigscreenplayer/playbackstrategy/nativehtml5strategy',
   [
     'bigscreenplayer/playbackstrategy/legacyplayeradapter',
     'bigscreenplayer/models/windowtypes',
-    'bigscreenplayer/playbackstrategy/modifiers/html5'
+    'bigscreenplayer/playbackstrategy/modifiers/html5',
+    'bigscreenplayer/playbackstrategy/modifiers/live/seekable'
   ],
-  function (LegacyAdapter, WindowTypes, Html5Player) {
+  function (LegacyAdapter, WindowTypes, Html5Player, LiveSeekablePlayer) {
     return function (windowType, mediaKind, timeData, playbackElement, isUHD, config) {
       var mediaPlayer;
-      config = {};
+      config = {}; // JUST FOR TESTING be warned
 
       if (windowType === WindowTypes.STATIC) {
         mediaPlayer = new Html5Player();
       } else {
-        mediaPlayer = undefined; // device.getLivePlayer();
+        mediaPlayer = new LiveSeekablePlayer();
       }
 
       return LegacyAdapter(windowType, mediaKind, timeData, playbackElement, isUHD, config, mediaPlayer);
