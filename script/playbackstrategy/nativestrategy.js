97ef1a40508e5219955b507e8909758135211ca7
Jacob Bracken <bracken.jacob3@gmail.co.uk>
2019-01-28 15:02:39 +0000

    remove unneeded "or" assignments

--- script/playbackstrategy/nativestrategy.js
+++ script/playbackstrategy/nativestrategy.js
@@ -3,18 +3,18 @@ define('bigscreenplayer/playbackstrategy/nativestrategy',
     'bigscreenplayer/playbackstrategy/legacyplayeradapter',
     'bigscreenplayer/models/windowtypes',
     'bigscreenplayer/playbackstrategy/modifiers/html5',
-    'bigscreenplayer/playbackstrategy/modifiers/live/' + (window.bigscreenPlayer.liveSupport || 'playable')
+    'bigscreenplayer/playbackstrategy/modifiers/live/' + window.bigscreenPlayer.liveSupport
   ],
   function (LegacyAdapter, WindowTypes, Html5Player, LivePlayer) {
     return function (windowType, mediaKind, timeData, playbackElement, isUHD, device) {
       var mediaPlayer;
       var logger = device.getLogger();
-      var tempConfig = device.getConfig() || {};
+      var tempConfig = device.getConfig();
 
-      if (windowType === WindowTypes.STATIC) {
-        mediaPlayer = Html5Player(logger);
-      } else {
+      if (windowType !== WindowTypes.STATIC) {
         mediaPlayer = LivePlayer(tempConfig, logger);
+      } else {
+        mediaPlayer = Html5Player(logger);
       }
 
       return LegacyAdapter(windowType, mediaKind, timeData, playbackElement, isUHD, device._config, mediaPlayer);
