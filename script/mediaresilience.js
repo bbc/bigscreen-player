1b064bfd884090bea9e3cea4e901a7645ce85579
magnuswoody <magnus.woodgate@bbc.co.uk>
2019-07-04 11:24:04 +0100

    Allowed restartable native devices to failover

--- script/mediaresilience.js
+++ script/mediaresilience.js
@@ -1,16 +1,17 @@
 define(
   'bigscreenplayer/mediaresilience', [
     'bigscreenplayer/models/windowtypes',
+    'bigscreenplayer/models/playbackstrategy',
     'bigscreenplayer/models/livesupport',
     'bigscreenplayer/models/transferformats'
   ],
-  function (WindowTypes, LiveSupport, TransferFormats) {
+  function (WindowTypes, PlaybackStrategy, LiveSupport, TransferFormats) {
     'use strict';
 
     function shouldFailover (remainingUrls, duration, currentTime, liveSupport, windowType, transferFormat) {
       var aboutToEnd = duration && currentTime > duration - 5;
       var shouldStaticFailover = windowType === WindowTypes.STATIC && !aboutToEnd;
-      var shouldLiveFailover = windowType !== WindowTypes.STATIC && (transferFormat === TransferFormats.DASH || liveSupport !== LiveSupport.RESTARTABLE);
+      var shouldLiveFailover = windowType !== WindowTypes.STATIC && window.bigscreenPlayer.playbackStrategy !== PlaybackStrategy.TAL;
       return remainingUrls > 1 && (shouldStaticFailover || shouldLiveFailover);
     }
 
