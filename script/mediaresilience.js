8a73d1422a9e667c4ef3b3f8e5fdd8f83f455f8e
magnuswoody <magnus.woodgate@bbc.co.uk>
2019-07-10 14:47:30 +0100

    Added check for disableLiveFailover window config flag

--- script/mediaresilience.js
+++ script/mediaresilience.js
@@ -1,17 +1,15 @@
 define(
   'bigscreenplayer/mediaresilience', [
     'bigscreenplayer/models/windowtypes',
-    'bigscreenplayer/models/playbackstrategy',
-    'bigscreenplayer/models/livesupport',
-    'bigscreenplayer/models/transferformats'
+    'bigscreenplayer/models/playbackstrategy'
   ],
-  function (WindowTypes, PlaybackStrategy, LiveSupport, TransferFormats) {
+  function (WindowTypes, PlaybackStrategy) {
     'use strict';
 
     function shouldFailover (remainingUrls, duration, currentTime, liveSupport, windowType, transferFormat) {
       var aboutToEnd = duration && currentTime > duration - 5;
       var shouldStaticFailover = windowType === WindowTypes.STATIC && !aboutToEnd;
-      var shouldLiveFailover = windowType !== WindowTypes.STATIC && window.bigscreenPlayer.playbackStrategy !== PlaybackStrategy.TAL;
+      var shouldLiveFailover = windowType !== WindowTypes.STATIC && window.bigscreenPlayer.playbackStrategy !== PlaybackStrategy.TAL && !window.bigscreenPlayer.disableLiveFailover;
       return remainingUrls > 1 && (shouldStaticFailover || shouldLiveFailover);
     }
 
