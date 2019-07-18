99c79cd1f4724a5a6fd4efef9c1dd918a4a66222
magnuswoody <magnus.woodgate@bbc.co.uk>
2019-07-18 09:54:45 +0100

    Added method of enabling cehtml on nativestrategy devices

--- script/playbackstrategy/nativestrategy.js
+++ script/playbackstrategy/nativestrategy.js
@@ -2,15 +2,15 @@ define('bigscreenplayer/playbackstrategy/nativestrategy',
   [
     'bigscreenplayer/playbackstrategy/legacyplayeradapter',
     'bigscreenplayer/models/windowtypes',
-    'bigscreenplayer/playbackstrategy/modifiers/html5',
+    'bigscreenplayer/playbackstrategy/modifiers/' + (window.bigscreenPlayer.mediaPlayer || 'html5'),
     'bigscreenplayer/playbackstrategy/modifiers/live/' + (window.bigscreenPlayer.liveSupport || 'playable')
   ],
-  function (LegacyAdapter, WindowTypes, Html5Player, LivePlayer) {
+  function (LegacyAdapter, WindowTypes, MediaPlayer, LivePlayer) {
     var NativeStrategy = function (windowType, mediaKind, timeData, playbackElement, isUHD, device) {
       var mediaPlayer;
       var tempConfig = device.getConfig();
 
-      mediaPlayer = Html5Player(tempConfig);
+      mediaPlayer = MediaPlayer(window.bigscreenPlayer.mediaPlayer === 'html5' ? tempConfig : device);
       if (windowType !== WindowTypes.STATIC) {
         mediaPlayer = LivePlayer(mediaPlayer, tempConfig, windowType, timeData);
       }
