2cac2d69f09405a96a46cbf2af94a5c4009092f8
Joel Keers <joel.keers@bbc.co.uk>
2019-11-12 14:35:55 +0000

    improve calculateSlidingWindowSeekOffset test clarity

--- script-test/utils/timeutilstest.js
+++ script-test/utils/timeutilstest.js
@@ -28,7 +28,7 @@ require(
 
       describe('Calculate Sliding Window Seek Offset', function () {
         beforeEach(function () {
-          jasmine.clock().mockDate(new Date('2019-10-22T10:55:24.952Z'));
+          jasmine.clock().mockDate(new Date('2019-10-22T10:59:20.000Z'));
         });
 
         afterEach(function () {
@@ -36,22 +36,27 @@ require(
         });
 
         it('should return the relative time in seconds including the time a user spent seeking', function () {
-          var time = 4895.142446990982;
-          var dvrInfoRangeStart = new Date('2019-10-22T08:54:24.006Z') / 1000;
-          var timeCorrection = new Date('2019-10-22T08:29:20.247Z') / 1000;
-          var pausedTime = new Date('2019-10-22T10:55:24.045Z');
+          var time = 4000;
 
-          expect(TimeUtils.calculateSlidingWindowSeekOffset(time, dvrInfoRangeStart, timeCorrection, pausedTime)).toBe(3390.476446931839);
+          // Note the 5 minute (300 second difference)
+          var dvrInfoRangeStart = new Date('2019-10-22T09:00:00.000Z') / 1000;
+          var timeCorrection = new Date('2019-10-22T08:55:00.000Z') / 1000;
+
+          var pausedTime = new Date('2019-10-22T10:59:00.000Z');
+          // mock Date.now is 20 seconds later. Slow seeking!
+
+          expect(TimeUtils.calculateSlidingWindowSeekOffset(time, dvrInfoRangeStart, timeCorrection, pausedTime)).toBe(3680);
         });
 
         it('should return the relative time in seconds if paused time is 0', function () {
-          var time = 4895.142446990982;
-          var dvrInfoRangeStart = new Date('2019-10-22T08:54:24.006Z') / 1000;
-          var timeCorrection = new Date('2019-10-22T08:29:20.247Z') / 1000;
-          var expectedTime = time + timeCorrection - dvrInfoRangeStart;
+          var time = 4000;
+
+          // Note the 5 minute (300 second difference)
+          var dvrInfoRangeStart = new Date('2019-10-22T09:00:00.000Z') / 1000;
+          var timeCorrection = new Date('2019-10-22T08:55:00.000Z') / 1000;
           var pausedTime = 0;
 
-          expect(TimeUtils.calculateSlidingWindowSeekOffset(time, dvrInfoRangeStart, timeCorrection, pausedTime)).toBe(expectedTime);
+          expect(TimeUtils.calculateSlidingWindowSeekOffset(time, dvrInfoRangeStart, timeCorrection, pausedTime)).toBe(3700);
         });
       });
     });
