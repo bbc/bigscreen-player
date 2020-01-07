a1722a2e57db0c905996f9a6f2286ff8eb869dd0
Joel Keers <joel.keers@bbc.co.uk>
2020-01-07 10:52:21 +0000

    update buildVersion script

--- buildVersion.js
+++ buildVersion.js
@@ -1,21 +1,23 @@
-(function () {
-  'use strict';
-  const fs = require('fs');
-  const path = require('path');
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const packagePath = path.resolve() + '/package.json';
+const versionFilePath = path.resolve() + '/script/version.js';
 
-  const packagePath = path.resolve() + '/package.json';
-  const versionFilePath = path.resolve() + '/script/version.js';
+fs.readFile(packagePath, (packageReadError, data) => {
+  if (packageReadError) throw packageReadError;
+  const packageVersion = JSON.parse(data).version;
+  const versionMatches = packageVersion.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
 
-  try {
-    const packageVersion = JSON.parse(fs.readFileSync(packagePath, 'utf8')).version;
-    const versionMatches = packageVersion.match(/[0-9]+\.[0-9]+\.[0-9]+/);
+  if (versionMatches) {
+    fs.readFile(versionFilePath, (versionReadError, data) => {
+      if (versionReadError) throw versionReadError;
+      var versionFile = data.toString().replace(/[0-9]+\.[0-9]+\.[0-9]/, versionMatches[0]);
 
-    if (versionMatches) {
-      var versionFile = fs.readFileSync(versionFilePath, 'utf8');
-      versionFile = versionFile.replace(/[0-9]+\.[0-9]+\.[0-9]+/, versionMatches[0]);
-      fs.writeFileSync(versionFilePath, versionFile);
-    }
-  } catch (e) {
-    console.error('Error setting version');
+      fs.writeFile(versionFilePath, versionFile, (writeError) => {
+        if (writeError) throw writeError;
+        console.log(`version updated to: ${versionMatches[0]} `);
+      });
+    });
   }
-})();
+});
