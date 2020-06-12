a1e0de5f107064c41cb7ae6f969858f28720fbdd
Joel Keers <joel.keers@bbc.co.uk>
2020-06-12 14:32:43 +0100

    do the most annoying thing and lint these files for spacing.

--- script/playbackstrategy/modifiers/live/restartable.js
+++ script/playbackstrategy/modifiers/live/restartable.js
@@ -1,157 +1,157 @@
 define(
-    'bigscreenplayer/playbackstrategy/modifiers/live/restartable',
+  'bigscreenplayer/playbackstrategy/modifiers/live/restartable',
   [
     'bigscreenplayer/playbackstrategy/modifiers/mediaplayerbase',
     'bigscreenplayer/models/windowtypes',
     'bigscreenplayer/dynamicwindowutils'
   ],
-    function (MediaPlayerBase, WindowTypes, DynamicWindowUtils) {
-      'use strict';
+  function (MediaPlayerBase, WindowTypes, DynamicWindowUtils) {
+    'use strict';
 
-      function RestartableLivePlayer (mediaPlayer, deviceConfig, windowType, mediaSources) {
-        var callbacksMap = [];
-        var startTime;
-        var fakeTimer = {};
-        var timeCorrection = mediaSources.time().correction || 0;
-        addEventCallback(this, updateFakeTimer);
+    function RestartableLivePlayer (mediaPlayer, deviceConfig, windowType, mediaSources) {
+      var callbacksMap = [];
+      var startTime;
+      var fakeTimer = {};
+      var timeCorrection = mediaSources.time().correction || 0;
+      addEventCallback(this, updateFakeTimer);
 
-        function updateFakeTimer (event) {
-          if (fakeTimer.wasPlaying && fakeTimer.runningTime) {
-            fakeTimer.currentTime += (Date.now() - fakeTimer.runningTime) / 1000;
-          }
-
-          fakeTimer.runningTime = Date.now();
-          fakeTimer.wasPlaying = event.state === MediaPlayerBase.STATE.PLAYING;
+      function updateFakeTimer (event) {
+        if (fakeTimer.wasPlaying && fakeTimer.runningTime) {
+          fakeTimer.currentTime += (Date.now() - fakeTimer.runningTime) / 1000;
         }
 
-        function addEventCallback (thisArg, callback) {
-          function newCallback (event) {
-            event.currentTime = getCurrentTime();
-            event.seekableRange = getSeekableRange();
-            callback(event);
-          }
-          callbacksMap.push({ from: callback, to: newCallback });
-          mediaPlayer.addEventCallback(thisArg, newCallback);
+        fakeTimer.runningTime = Date.now();
+        fakeTimer.wasPlaying = event.state === MediaPlayerBase.STATE.PLAYING;
+      }
+
+      function addEventCallback (thisArg, callback) {
+        function newCallback (event) {
+          event.currentTime = getCurrentTime();
+          event.seekableRange = getSeekableRange();
+          callback(event);
         }
+        callbacksMap.push({ from: callback, to: newCallback });
+        mediaPlayer.addEventCallback(thisArg, newCallback);
+      }
 
-        function removeEventCallback (thisArg, callback) {
-          var filteredCallbacks = callbacksMap.filter(function (cb) {
-            return cb.from === callback;
-          });
+      function removeEventCallback (thisArg, callback) {
+        var filteredCallbacks = callbacksMap.filter(function (cb) {
+          return cb.from === callback;
+        });
 
-          if (filteredCallbacks.length > 0) {
-            callbacksMap = callbacksMap.splice(callbacksMap.indexOf(filteredCallbacks[0]));
+        if (filteredCallbacks.length > 0) {
+          callbacksMap = callbacksMap.splice(callbacksMap.indexOf(filteredCallbacks[0]));
 
-            mediaPlayer.removeEventCallback(thisArg, filteredCallbacks[0].to);
-          }
+          mediaPlayer.removeEventCallback(thisArg, filteredCallbacks[0].to);
         }
+      }
 
-        function removeAllEventCallbacks () {
-          mediaPlayer.removeAllEventCallbacks();
+      function removeAllEventCallbacks () {
+        mediaPlayer.removeAllEventCallbacks();
+      }
+
+      function resume () {
+        mediaPlayer.resume();
+      }
+
+      function pause (opts) {
+        mediaPlayer.pause();
+        opts = opts || {};
+        if (opts.disableAutoResume !== true) {
+          DynamicWindowUtils.autoResumeAtStartOfRange(
+            getCurrentTime(),
+            getSeekableRange(),
+            addEventCallback,
+            removeEventCallback,
+            MediaPlayerBase.unpausedEventCheck,
+            resume);
         }
+      }
 
-        function resume () {
-          mediaPlayer.resume();
-        }
+      function getCurrentTime () {
+        return fakeTimer.currentTime + timeCorrection;
+      }
 
-        function pause (opts) {
-          mediaPlayer.pause();
-          opts = opts || {};
-          if (opts.disableAutoResume !== true) {
-            DynamicWindowUtils.autoResumeAtStartOfRange(
-              getCurrentTime(),
-              getSeekableRange(),
-              addEventCallback,
-              removeEventCallback,
-              MediaPlayerBase.unpausedEventCheck,
-              resume);
-          }
-        }
-
-        function getCurrentTime () {
-          return fakeTimer.currentTime + timeCorrection;
-        }
-
-        function getSeekableRange () {
-          var windowLength = (mediaSources.time().windowEndTime - mediaSources.time().windowStartTime) / 1000;
-          var delta = (Date.now() - startTime) / 1000;
-          return {
-            start: (windowType === WindowTypes.SLIDING ? delta : 0) + timeCorrection,
-            end: windowLength + delta + timeCorrection
-          };
-        }
-
+      function getSeekableRange () {
+        var windowLength = (mediaSources.time().windowEndTime - mediaSources.time().windowStartTime) / 1000;
+        var delta = (Date.now() - startTime) / 1000;
         return {
-          beginPlayback: function () {
-            var config = deviceConfig;
-
-            startTime = Date.now();
-            fakeTimer.currentTime = (mediaSources.time().windowEndTime - mediaSources.time().windowStartTime) / 1000;
-
-            if (config && config.streaming && config.streaming.overrides && config.streaming.overrides.forceBeginPlaybackToEndOfWindow) {
-              mediaPlayer.beginPlaybackFrom(Infinity);
-            } else {
-              mediaPlayer.beginPlayback();
-            }
-          },
-
-          beginPlaybackFrom: function (offset) {
-            startTime = Date.now();
-            fakeTimer.currentTime = offset;
-            mediaPlayer.beginPlaybackFrom(offset);
-          },
-
-          initialiseMedia: function (mediaType, sourceUrl, mimeType, sourceContainer, opts) {
-            if (mediaType === MediaPlayerBase.TYPE.AUDIO) {
-              mediaType = MediaPlayerBase.TYPE.LIVE_AUDIO;
-            } else {
-              mediaType = MediaPlayerBase.TYPE.LIVE_VIDEO;
-            }
-
-            mediaPlayer.initialiseMedia(mediaType, sourceUrl, mimeType, sourceContainer, opts);
-          },
-
-          pause: pause,
-
-          resume: resume,
-
-          stop: function () {
-            mediaPlayer.stop();
-          },
-
-          reset: function () {
-            mediaPlayer.reset();
-          },
-
-          getState: function () {
-            return mediaPlayer.getState();
-          },
-
-          getSource: function () {
-            return mediaPlayer.getSource();
-          },
-
-          getMimeType: function () {
-            return mediaPlayer.getMimeType();
-          },
-
-          addEventCallback: addEventCallback,
-
-          removeEventCallback: removeEventCallback,
-
-          removeAllEventCallbacks: removeAllEventCallbacks,
-
-          getPlayerElement: function () {
-            return mediaPlayer.getPlayerElement();
-          },
-
-          getCurrentTime: getCurrentTime,
-
-          getSeekableRange: getSeekableRange
-
+          start: (windowType === WindowTypes.SLIDING ? delta : 0) + timeCorrection,
+          end: windowLength + delta + timeCorrection
         };
       }
 
-      return RestartableLivePlayer;
+      return {
+        beginPlayback: function () {
+          var config = deviceConfig;
+
+          startTime = Date.now();
+          fakeTimer.currentTime = (mediaSources.time().windowEndTime - mediaSources.time().windowStartTime) / 1000;
+
+          if (config && config.streaming && config.streaming.overrides && config.streaming.overrides.forceBeginPlaybackToEndOfWindow) {
+            mediaPlayer.beginPlaybackFrom(Infinity);
+          } else {
+            mediaPlayer.beginPlayback();
+          }
+        },
+
+        beginPlaybackFrom: function (offset) {
+          startTime = Date.now();
+          fakeTimer.currentTime = offset;
+          mediaPlayer.beginPlaybackFrom(offset);
+        },
+
+        initialiseMedia: function (mediaType, sourceUrl, mimeType, sourceContainer, opts) {
+          if (mediaType === MediaPlayerBase.TYPE.AUDIO) {
+            mediaType = MediaPlayerBase.TYPE.LIVE_AUDIO;
+          } else {
+            mediaType = MediaPlayerBase.TYPE.LIVE_VIDEO;
+          }
+
+          mediaPlayer.initialiseMedia(mediaType, sourceUrl, mimeType, sourceContainer, opts);
+        },
+
+        pause: pause,
+
+        resume: resume,
+
+        stop: function () {
+          mediaPlayer.stop();
+        },
+
+        reset: function () {
+          mediaPlayer.reset();
+        },
+
+        getState: function () {
+          return mediaPlayer.getState();
+        },
+
+        getSource: function () {
+          return mediaPlayer.getSource();
+        },
+
+        getMimeType: function () {
+          return mediaPlayer.getMimeType();
+        },
+
+        addEventCallback: addEventCallback,
+
+        removeEventCallback: removeEventCallback,
+
+        removeAllEventCallbacks: removeAllEventCallbacks,
+
+        getPlayerElement: function () {
+          return mediaPlayer.getPlayerElement();
+        },
+
+        getCurrentTime: getCurrentTime,
+
+        getSeekableRange: getSeekableRange
+
+      };
     }
+
+    return RestartableLivePlayer;
+  }
 );
--- script/playbackstrategy/modifiers/mediaplayerbase.js
+++ script/playbackstrategy/modifiers/mediaplayerbase.js
@@ -1,56 +1,56 @@
 define(
-    'bigscreenplayer/playbackstrategy/modifiers/mediaplayerbase',
-    function () {
-      var STATE = {
-        EMPTY: 'EMPTY',     // No source set
-        STOPPED: 'STOPPED',   // Source set but no playback
-        BUFFERING: 'BUFFERING', // Not enough data to play, waiting to download more
-        PLAYING: 'PLAYING',   // Media is playing
-        PAUSED: 'PAUSED',    // Media is paused
-        COMPLETE: 'COMPLETE',  // Media has reached its end point
-        ERROR: 'ERROR'      // An error occurred
-      };
+  'bigscreenplayer/playbackstrategy/modifiers/mediaplayerbase',
+  function () {
+    var STATE = {
+      EMPTY: 'EMPTY', // No source set
+      STOPPED: 'STOPPED', // Source set but no playback
+      BUFFERING: 'BUFFERING', // Not enough data to play, waiting to download more
+      PLAYING: 'PLAYING', // Media is playing
+      PAUSED: 'PAUSED', // Media is paused
+      COMPLETE: 'COMPLETE', // Media has reached its end point
+      ERROR: 'ERROR' // An error occurred
+    };
 
-      var EVENT = {
-        STOPPED: 'stopped',   // Event fired when playback is stopped
-        BUFFERING: 'buffering', // Event fired when playback has to suspend due to buffering
-        PLAYING: 'playing',   // Event fired when starting (or resuming) playing of the media
-        PAUSED: 'paused',    // Event fired when media playback pauses
-        COMPLETE: 'complete',  // Event fired when media playback has reached the end of the media
-        ERROR: 'error',     // Event fired when an error condition occurs
-        STATUS: 'status',    // Event fired regularly during play
-        SENTINEL_ENTER_BUFFERING: 'sentinel-enter-buffering', // Event fired when a sentinel has to act because the device has started buffering but not reported it
-        SENTINEL_EXIT_BUFFERING: 'sentinel-exit-buffering',  // Event fired when a sentinel has to act because the device has finished buffering but not reported it
-        SENTINEL_PAUSE: 'sentinel-pause',           // Event fired when a sentinel has to act because the device has failed to pause when expected
-        SENTINEL_PLAY: 'sentinel-play',            // Event fired when a sentinel has to act because the device has failed to play when expected
-        SENTINEL_SEEK: 'sentinel-seek',            // Event fired when a sentinel has to act because the device has failed to seek to the correct location
-        SENTINEL_COMPLETE: 'sentinel-complete',        // Event fired when a sentinel has to act because the device has completed the media but not reported it
-        SENTINEL_PAUSE_FAILURE: 'sentinel-pause-failure',   // Event fired when the pause sentinel has failed twice, so it is giving up
-        SENTINEL_SEEK_FAILURE: 'sentinel-seek-failure',     // Event fired when the seek sentinel has failed twice, so it is giving up
-        SEEK_ATTEMPTED: 'seek-attempted', // Event fired when a device using a seekfinishedemitevent modifier sets the source
-        SEEK_FINISHED: 'seek-finished'    // Event fired when a device using a seekfinishedemitevent modifier has seeked successfully
-      };
+    var EVENT = {
+      STOPPED: 'stopped', // Event fired when playback is stopped
+      BUFFERING: 'buffering', // Event fired when playback has to suspend due to buffering
+      PLAYING: 'playing', // Event fired when starting (or resuming) playing of the media
+      PAUSED: 'paused', // Event fired when media playback pauses
+      COMPLETE: 'complete', // Event fired when media playback has reached the end of the media
+      ERROR: 'error', // Event fired when an error condition occurs
+      STATUS: 'status', // Event fired regularly during play
+      SENTINEL_ENTER_BUFFERING: 'sentinel-enter-buffering', // Event fired when a sentinel has to act because the device has started buffering but not reported it
+      SENTINEL_EXIT_BUFFERING: 'sentinel-exit-buffering', // Event fired when a sentinel has to act because the device has finished buffering but not reported it
+      SENTINEL_PAUSE: 'sentinel-pause', // Event fired when a sentinel has to act because the device has failed to pause when expected
+      SENTINEL_PLAY: 'sentinel-play', // Event fired when a sentinel has to act because the device has failed to play when expected
+      SENTINEL_SEEK: 'sentinel-seek', // Event fired when a sentinel has to act because the device has failed to seek to the correct location
+      SENTINEL_COMPLETE: 'sentinel-complete', // Event fired when a sentinel has to act because the device has completed the media but not reported it
+      SENTINEL_PAUSE_FAILURE: 'sentinel-pause-failure', // Event fired when the pause sentinel has failed twice, so it is giving up
+      SENTINEL_SEEK_FAILURE: 'sentinel-seek-failure', // Event fired when the seek sentinel has failed twice, so it is giving up
+      SEEK_ATTEMPTED: 'seek-attempted', // Event fired when a device using a seekfinishedemitevent modifier sets the source
+      SEEK_FINISHED: 'seek-finished' // Event fired when a device using a seekfinishedemitevent modifier has seeked successfully
+    };
 
-      var TYPE = {
-        VIDEO: 'video',
-        AUDIO: 'audio',
-        LIVE_VIDEO: 'live-video',
-        LIVE_AUDIO: 'live-audio'
-      };
+    var TYPE = {
+      VIDEO: 'video',
+      AUDIO: 'audio',
+      LIVE_VIDEO: 'live-video',
+      LIVE_AUDIO: 'live-audio'
+    };
 
-      function unpausedEventCheck (event) {
-        if (event && event.state) {
-          return event.state !== STATE.PAUSED;
-        } else {
-          return undefined;
-        }
+    function unpausedEventCheck (event) {
+      if (event && event.state) {
+        return event.state !== STATE.PAUSED;
+      } else {
+        return undefined;
       }
-
-      return {
-        STATE: STATE,
-        EVENT: EVENT,
-        TYPE: TYPE,
-        unpausedEventCheck: unpausedEventCheck
-      };
     }
+
+    return {
+      STATE: STATE,
+      EVENT: EVENT,
+      TYPE: TYPE,
+      unpausedEventCheck: unpausedEventCheck
+    };
+  }
 );
