c16e3f08bf7667fdd39166f0f8eabf9b081ca274
Joel Keers <joel.keers@bbc.co.uk>
2020-06-16 13:42:18 +0100

    fix linting from the merge

--- script/playbackstrategy/modifiers/live/seekable.js
+++ script/playbackstrategy/modifiers/live/seekable.js
@@ -5,55 +5,77 @@ define(
     'bigscreenplayer/dynamicwindowutils',
     'bigscreenplayer/models/windowtypes'
   ],
-    function (MediaPlayerBase, DynamicWindowUtils, WindowTypes) {
-      'use strict';
-      function SeekableLivePlayer (mediaPlayer, deviceConfig, windowType) {
-        var AUTO_RESUME_WINDOW_START_CUSHION_SECONDS = 8;
+  function (MediaPlayerBase, DynamicWindowUtils, WindowTypes) {
+    'use strict';
+    function SeekableLivePlayer (mediaPlayer, deviceConfig, windowType) {
+      var AUTO_RESUME_WINDOW_START_CUSHION_SECONDS = 8;
 
-        function addEventCallback (thisArg, callback) {
-          mediaPlayer.addEventCallback(thisArg, callback);
-        }
+      function addEventCallback (thisArg, callback) {
+        mediaPlayer.addEventCallback(thisArg, callback);
+      }
 
-        function removeEventCallback (thisArg, callback) {
-          mediaPlayer.removeEventCallback(thisArg, callback);
-        }
+      function removeEventCallback (thisArg, callback) {
+        mediaPlayer.removeEventCallback(thisArg, callback);
+      }
 
-        function removeAllEventCallbacks () {
-          mediaPlayer.removeAllEventCallbacks();
-        }
+      function removeAllEventCallbacks () {
+        mediaPlayer.removeAllEventCallbacks();
+      }
 
-        function resume () {
-          mediaPlayer.resume();
-        }
+      function resume () {
+        mediaPlayer.resume();
+      }
 
-        return ({
-          initialiseMedia: function initialiseMedia (mediaType, sourceUrl, mimeType, sourceContainer, opts) {
-            if (mediaType === MediaPlayerBase.TYPE.AUDIO) {
-              mediaType = MediaPlayerBase.TYPE.LIVE_AUDIO;
-            } else {
-              mediaType = MediaPlayerBase.TYPE.LIVE_VIDEO;
-            }
+      return ({
+        initialiseMedia: function initialiseMedia (mediaType, sourceUrl, mimeType, sourceContainer, opts) {
+          if (mediaType === MediaPlayerBase.TYPE.AUDIO) {
+            mediaType = MediaPlayerBase.TYPE.LIVE_AUDIO;
+          } else {
+            mediaType = MediaPlayerBase.TYPE.LIVE_VIDEO;
+          }
 
-            mediaPlayer.initialiseMedia(mediaType, sourceUrl, mimeType, sourceContainer, opts);
-          },
+          mediaPlayer.initialiseMedia(mediaType, sourceUrl, mimeType, sourceContainer, opts);
+        },
 
-          beginPlayback: function beginPlayback () {
-            var config = deviceConfig;
-            if (config && config.streaming && config.streaming.overrides && config.streaming.overrides.forceBeginPlaybackToEndOfWindow) {
-              mediaPlayer.beginPlaybackFrom(Infinity);
-            } else {
-              mediaPlayer.beginPlayback();
-            }
-          },
+        beginPlayback: function beginPlayback () {
+          var config = deviceConfig;
+          if (config && config.streaming && config.streaming.overrides && config.streaming.overrides.forceBeginPlaybackToEndOfWindow) {
+            mediaPlayer.beginPlaybackFrom(Infinity);
+          } else {
+            mediaPlayer.beginPlayback();
+          }
+        },
 
-          beginPlaybackFrom: function beginPlaybackFrom (offset) {
-            mediaPlayer.beginPlaybackFrom(offset);
-          },
+        beginPlaybackFrom: function beginPlaybackFrom (offset) {
+          mediaPlayer.beginPlaybackFrom(offset);
+        },
 
-          playFrom: function playFrom (offset) {
-            var postSeekState = mediaPlayer.getState();
-            mediaPlayer.playFrom(offset);
-            if (postSeekState === MediaPlayerBase.STATE.PAUSED && windowType === WindowTypes.SLIDING) {
+        playFrom: function playFrom (offset) {
+          var postSeekState = mediaPlayer.getState();
+          mediaPlayer.playFrom(offset);
+          if (postSeekState === MediaPlayerBase.STATE.PAUSED && windowType === WindowTypes.SLIDING) {
+            DynamicWindowUtils.autoResumeAtStartOfRange(
+              mediaPlayer.getCurrentTime(),
+              mediaPlayer.getSeekableRange(),
+              addEventCallback,
+              removeEventCallback,
+              MediaPlayerBase.unpausedEventCheck,
+              resume);
+          }
+        },
+
+        pause: function pause (opts) {
+          opts = opts || {};
+          var secondsUntilStartOfWindow = mediaPlayer.getCurrentTime() - mediaPlayer.getSeekableRange().start;
+
+          if (opts.disableAutoResume) {
+            mediaPlayer.pause();
+          } else if (secondsUntilStartOfWindow <= AUTO_RESUME_WINDOW_START_CUSHION_SECONDS) {
+            mediaPlayer.toPaused();
+            mediaPlayer.toPlaying();
+          } else {
+            mediaPlayer.pause();
+            if (windowType === WindowTypes.SLIDING) {
               DynamicWindowUtils.autoResumeAtStartOfRange(
                 mediaPlayer.getCurrentTime(),
                 mediaPlayer.getSeekableRange(),
@@ -62,75 +84,53 @@ define(
                 MediaPlayerBase.unpausedEventCheck,
                 resume);
             }
-          },
-
-          pause: function pause (opts) {
-            opts = opts || {};
-            var secondsUntilStartOfWindow = mediaPlayer.getCurrentTime() - mediaPlayer.getSeekableRange().start;
-
-            if (opts.disableAutoResume) {
-              mediaPlayer.pause();
-            } else if (secondsUntilStartOfWindow <= AUTO_RESUME_WINDOW_START_CUSHION_SECONDS) {
-              mediaPlayer.toPaused();
-              mediaPlayer.toPlaying();
-            } else {
-              mediaPlayer.pause();
-              if (windowType === WindowTypes.SLIDING) {
-                DynamicWindowUtils.autoResumeAtStartOfRange(
-                  mediaPlayer.getCurrentTime(),
-                  mediaPlayer.getSeekableRange(),
-                  addEventCallback,
-                  removeEventCallback,
-                  MediaPlayerBase.unpausedEventCheck,
-                  resume);
-              }
-            }
-          },
-          resume: resume,
-
-          stop: function stop () {
-            mediaPlayer.stop();
-          },
-
-          reset: function reset () {
-            mediaPlayer.reset();
-          },
-
-          getState: function getState () {
-            return mediaPlayer.getState();
-          },
-
-          getSource: function getSource () {
-            return mediaPlayer.getSource();
-          },
-
-          getCurrentTime: function getCurrentTime () {
-            return mediaPlayer.getCurrentTime();
-          },
-
-          getSeekableRange: function getSeekableRange () {
-            return mediaPlayer.getSeekableRange();
-          },
-
-          getMimeType: function getMimeType () {
-            return mediaPlayer.getMimeType();
-          },
-
-          addEventCallback: addEventCallback,
-
-          removeEventCallback: removeEventCallback,
-
-          removeAllEventCallbacks: removeAllEventCallbacks,
-
-          getPlayerElement: function getPlayerElement () {
-            return mediaPlayer.getPlayerElement();
-          },
-
-          getLiveSupport: function getLiveSupport () {
-            return MediaPlayerBase.LIVE_SUPPORT.SEEKABLE;
           }
-        });
-      }
+        },
+        resume: resume,
 
-      return SeekableLivePlayer;
-    });
+        stop: function stop () {
+          mediaPlayer.stop();
+        },
+
+        reset: function reset () {
+          mediaPlayer.reset();
+        },
+
+        getState: function getState () {
+          return mediaPlayer.getState();
+        },
+
+        getSource: function getSource () {
+          return mediaPlayer.getSource();
+        },
+
+        getCurrentTime: function getCurrentTime () {
+          return mediaPlayer.getCurrentTime();
+        },
+
+        getSeekableRange: function getSeekableRange () {
+          return mediaPlayer.getSeekableRange();
+        },
+
+        getMimeType: function getMimeType () {
+          return mediaPlayer.getMimeType();
+        },
+
+        addEventCallback: addEventCallback,
+
+        removeEventCallback: removeEventCallback,
+
+        removeAllEventCallbacks: removeAllEventCallbacks,
+
+        getPlayerElement: function getPlayerElement () {
+          return mediaPlayer.getPlayerElement();
+        },
+
+        getLiveSupport: function getLiveSupport () {
+          return MediaPlayerBase.LIVE_SUPPORT.SEEKABLE;
+        }
+      });
+    }
+
+    return SeekableLivePlayer;
+  });
