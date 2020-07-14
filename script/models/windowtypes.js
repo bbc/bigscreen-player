d80d6ddc89cc2636202719459156ee48c143d48b
Joel Keers <joel.keers@bbc.co.uk>
2020-07-14 22:44:40 +0100

    add initial docs for bigscreenplayer

--- script/models/windowtypes.js
+++ script/models/windowtypes.js
@@ -1,10 +1,18 @@
+/**
+ * @module bigscreenplayer/models/windowtypes
+ * @readonly
+ * @enum {string}
+ */
 define(
   'bigscreenplayer/models/windowtypes',
   function () {
     'use strict';
-    return {
+    return /** @alias module:bigscreenplayer/models/windowtypes */ {
+      /** Media with a duration */
       STATIC: 'staticWindow',
+      /** Media with a start without an initial duration */
       GROWING: 'growingWindow',
+      /** Media with a rewind window that progresses through a media timeline */
       SLIDING: 'slidingWindow'
     };
   });
