969eb64c181f667371ec8fa4c688ab896fc9f9f4
Joel Keers <joel.keers@bbc.co.uk>
2020-08-20 15:30:51 +0100

    use Squire to mock out LoadUrl

--- script-test/subtitles/renderer.js
+++ script-test/subtitles/renderer.js
@@ -1,7 +1,22 @@
 require(
-  ['bigscreenplayer/subtitles/renderer'],
-  function (Renderer) {
+  [
+    'squire'
+  ],
+  function (Squire) {
     describe('Renderer', function () {
+      var Renderer;
+      var squire;
+      beforeEach(function (done) {
+        squire = new Squire();
+        squire.mock({
+          'bigscreenplayer/utils/loadurl': jasmine.createSpy()
+        });
+        squire.require(['bigscreenplayer/subtitles/renderer'], function (RendererWithMocks) {
+          Renderer = RendererWithMocks;
+          done();
+        });
+      });
+
       it('should initialise with a url, id and media player', function () {
         var mockMediaPlayer = jasmine.createSpy();
         var renderer = Renderer('http://some-url', 'subtitlesOutputId', mockMediaPlayer);
