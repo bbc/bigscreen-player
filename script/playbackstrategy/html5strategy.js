45b12b809e5338ca0a1efe8ffe80afaedb712e78
Matt Stephenson <matt.stephenson@bbc.co.uk>
2020-09-09 09:46:41 +0100

    Move initial time setting to onLoadedMetaData

--- script/playbackstrategy/html5strategy.js
+++ script/playbackstrategy/html5strategy.js
@@ -121,13 +121,13 @@ define('bigscreenplayer/playbackstrategy/html5strategy',
 
       function onLoadedMetadata () {
         metaDataLoaded = true;
-      }
-
-      function onCanPlay () {
-        if (playFromTime && metaDataLoaded) {
+        if (playFromTime) {
           mediaElement.currentTime = getClampedTime(playFromTime, getSeekableRange()) + timeCorrection;
           playFromTime = undefined;
         }
+      }
+
+      function onCanPlay () {
         if (!isPaused()) {
           mediaElement.play();
         }
