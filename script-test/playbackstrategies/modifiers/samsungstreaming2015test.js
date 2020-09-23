ff26433cb2325a711d53dc713ef06a6f696fc8cb
jamierbower <jamie.reidbower@bbc.co.uk>
2020-09-23 13:53:24 +0100

    more tests for samsung streaming

---
+++ script-test/playbackstrategies/modifiers/samsungstreaming2015test.js
@@ -0,0 +1,11 @@
+require(
+  [
+    'bigscreenplayer/playbackstrategy/modifiers/samsungstreaming2015',
+    'bigscreenplayer/playbackstrategy/modifiers/mediaplayerbase'
+  ],
+  function (Html5MediaPlayer, MediaPlayerBase) {
+    describe('Samsung Streaming 2015', function () {
+
+    });
+  }
+);
