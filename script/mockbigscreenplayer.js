07856ad53146ef466443cd980cc0857db6a7f36c
Joel Keers <joel.keers@bbc.co.uk>
2020-10-07 12:43:28 +0100

    actually assign the merged array

--- script/mockbigscreenplayer.js
+++ script/mockbigscreenplayer.js
@@ -82,7 +82,7 @@ define('bigscreenplayer/mockbigscreenplayer',
       autoProgress = opts && opts.autoProgress;
 
       if (opts && opts.excludedFuncs) {
-        PlaybackUtils.merge(excludedFuncs, opts.excludedFuncs);
+        excludedFuncs = PlaybackUtils.merge(excludedFuncs, opts.excludedFuncs);
       }
 
       if (mockStatus.currentlyMocked) {
@@ -109,7 +109,7 @@ define('bigscreenplayer/mockbigscreenplayer',
       autoProgress = opts && opts.autoProgress;
 
       if (opts && opts.excludedFuncs) {
-        PlaybackUtils.merge(excludedFuncs, opts.excludedFuncs);
+        excludedFuncs = PlaybackUtils.merge(excludedFuncs, opts.excludedFuncs);
       }
 
       if (mockStatus.currentlyMocked) {
