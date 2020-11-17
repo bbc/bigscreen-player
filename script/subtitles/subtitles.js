bded09bbebdc79c5c438c969107f035ebf4e57db
matt-stephenson <35135054+matt-stephenson@users.noreply.github.com>
2020-11-17 13:43:52 +0000

    Bug fix - only attempt to load subtitles file when there is a url to do so (#182)
    Co-authored-by: Joel Keers <joel.keers@bbc.co.uk>

--- script/subtitles/subtitles.js
+++ script/subtitles/subtitles.js
@@ -13,24 +13,26 @@ define('bigscreenplayer/subtitles/subtitles',
       var subtitlesEnabled = autoStart;
       var subtitlesAvailable = !!url;
 
-      DebugTool.info('Loading subtitles from: ' + url);
-      LoadURL(url, {
-        onLoad: function (responseXML, responseText, status) {
-          if (!responseXML) {
-            DebugTool.info('Error: responseXML is invalid.');
-            Plugins.interface.onSubtitlesTransformError();
-            return;
-          }
+      if (subtitlesAvailable) {
+        DebugTool.info('Loading subtitles from: ' + url);
+        LoadURL(url, {
+          onLoad: function (responseXML, responseText, status) {
+            if (!responseXML) {
+              DebugTool.info('Error: responseXML is invalid.');
+              Plugins.interface.onSubtitlesTransformError();
+              return;
+            }
 
-          if (status === 200) {
-            subtitlesContainer = SubtitlesContainer(mediaPlayer, responseXML, autoStart, playbackElement);
+            if (status === 200) {
+              subtitlesContainer = SubtitlesContainer(mediaPlayer, responseXML, autoStart, playbackElement);
+            }
+          },
+          onError: function (error) {
+            DebugTool.info('Error loading subtitles data: ' + error);
+            Plugins.interface.onSubtitlesLoadError();
           }
-        },
-        onError: function (error) {
-          DebugTool.info('Error loading subtitles data: ' + error);
-          Plugins.interface.onSubtitlesLoadError();
-        }
-      });
+        });
+      }
 
       function start () {
         subtitlesEnabled = true;
