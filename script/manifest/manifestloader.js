5830a5ddfab3395f9812afeac88a0f93b193f003
Peter Morris-Hind <peter.morris-hind@bbc.co.uk>
2021-03-15 11:11:16 +0000

    Trigger rebuild on Travis

--- script/manifest/manifestloader.js
+++ script/manifest/manifestloader.js
@@ -6,6 +6,7 @@ define(
   ],
   function (ManifestParser, TransferFormats, LoadUrl) {
     'use strict';
+
     function retrieveDashManifest (url, dateWithOffset, callbacks) {
       LoadUrl(
         url,
