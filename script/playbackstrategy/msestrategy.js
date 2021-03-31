8f6e4a77902e8d19395188fd4bcc96f3ea123dc7
Peter Morris-Hind <peter.morris-hind@bbc.co.uk>
2021-03-31 09:56:51 +0100

    Revert 476fcae010014aba0655caf710d7fae1283772fc

--- script/playbackstrategy/msestrategy.js
+++ script/playbackstrategy/msestrategy.js
@@ -57,8 +57,7 @@ define('bigscreenplayer/playbackstrategy/msestrategy',
         BASE_URL_SELECTED: 'baseUrlSelected',
         METRIC_ADDED: 'metricAdded',
         METRIC_CHANGED: 'metricChanged',
-        STREAM_INITIALIZED: 'streamInitialized',
-        PLAYBACK_ENDED: 'playbackEnded'
+        STREAM_INITIALIZED: 'streamInitialized'
       };
 
       function onPlaying () {
@@ -361,8 +360,8 @@ define('bigscreenplayer/playbackstrategy/msestrategy',
         mediaElement.addEventListener('waiting', onBuffering);
         mediaElement.addEventListener('seeking', onBuffering);
         mediaElement.addEventListener('seeked', onSeeked);
+        mediaElement.addEventListener('ended', onEnded);
         mediaElement.addEventListener('error', onError);
-        mediaPlayer.on(DashJSEvents.PLAYBACK_ENDED, onEnded);
         mediaPlayer.on(DashJSEvents.ERROR, onError);
         mediaPlayer.on(DashJSEvents.MANIFEST_LOADED, onManifestLoaded);
         mediaPlayer.on(DashJSEvents.STREAM_INITIALIZED, onStreamInitialised);
