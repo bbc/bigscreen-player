87fa713f10c76c80e0ca1e6481ad237b07932f7b
Peter Morris-Hind <peter.morris-hind@bbc.co.uk>
2021-04-01 09:55:43 +0100

    Back to Console Log

--- script/playbackstrategy/msestrategy.js
+++ script/playbackstrategy/msestrategy.js
@@ -392,9 +392,7 @@ define('bigscreenplayer/playbackstrategy/msestrategy',
           return startTime === 0 ? source : source + '#t=' + startTime;
         } else {
           var windowStartTimeSeconds = (mediaSources.time().windowStartTime / 1000);
-          DebugTool.keyValue({
-            key: 'windowStartTime',
-            value: windowStartTimeSeconds});
+          console.log('windowStartTime: ', mediaSources.time().windowStartTime);
           var srcWithTimeAnchor = source + '#t=posix:';
 
           return startTime === 0 ? srcWithTimeAnchor + (windowStartTimeSeconds + 1) : srcWithTimeAnchor + (windowStartTimeSeconds + startTime);
