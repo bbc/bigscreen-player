74eb5774fb44cf7df97c1e2aba61b17d1325a941
Luke Chatburn <luke.chatburn@bbc.co.uk>
2021-06-08 10:45:45 +0100

    Update buffer to keep

--- script/playbackstrategy/msestrategy.js
+++ script/playbackstrategy/msestrategy.js
@@ -336,7 +336,7 @@ define('bigscreenplayer/playbackstrategy/msestrategy',
           },
           streaming: {
             liveDelay: LIVE_DELAY_SECONDS,
-            bufferToKeep: 0,
+            bufferToKeep: 10,
             bufferTimeAtTopQuality: 12,
             bufferTimeAtTopQualityLongForm: 15
           }
