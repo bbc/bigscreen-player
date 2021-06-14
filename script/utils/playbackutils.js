f65d87f7629f61d43bfc32062b47d6635c996a4b
Luke Chatburn <luke.chatburn@bbc.co.uk>
2021-06-14 10:25:59 +0100

    Remove comments for utils

--- script/utils/playbackutils.js
+++ script/utils/playbackutils.js
@@ -53,10 +53,8 @@ define(
           for (var prop in obj) {
             if (obj.hasOwnProperty(prop)) {
               if (Object.prototype.toString.call(obj[prop]) === '[object Object]') {
-                // If we're doing a deep merge and the property is an object
                 merged[prop] = this.merge(merged[prop], obj[prop]);
               } else {
-                // Otherwise, do a regular merge
                 merged[prop] = obj[prop];
               }
             }
