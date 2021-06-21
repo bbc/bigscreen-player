2766b0bf10dca4fd4e17a3485de60a2a2c8efb8c
Tom Jenkinson <tom.jenkinson@bbc.co.uk>
2021-06-21 11:00:15 +0100

    fix import in test

--- script-test/utils/deferexceptiontest.js
+++ script-test/utils/deferexceptiontest.js
@@ -1,18 +1,18 @@
 require(
-  ['bigscreenplayer/utils/deferexception'],
-  function (deferException) {
+  ['bigscreenplayer/utils/deferexceptions'],
+  function (deferExceptions) {
     'use strict';
-    describe('deferException', function () {
+    describe('deferExceptions', function () {
       it('calls the callback once', function () {
         var callback = jasmine.createSpy('callback');
-        deferException(callback);
+        deferExceptions(callback);
 
         expect(callback).toHaveBeenCalledTimes(1);
       });
 
       it('does not let an exception through', function () {
         expect(function () {
-          deferException(function () {
+          deferExceptions(function () {
             throw new Error('oops');
           });
         }).not.toThrow();
