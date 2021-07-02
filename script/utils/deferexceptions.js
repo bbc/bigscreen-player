2d4c2e4d7528ee6df80e37b06569ceabb4bfdfdc
Joel Keers <joel.keers@bbc.co.uk>
2021-07-02 16:26:49 +0100

    DebugTool log caught deferred exceptions

--- script/utils/deferexceptions.js
+++ script/utils/deferexceptions.js
@@ -1,11 +1,13 @@
 define(
   'bigscreenplayer/utils/deferexceptions',
-  function () {
+  ['bigscreenplayer/debugger/debugtool'],
+  function (DebugTool) {
     'use strict';
     return function deferExceptions (cb) {
       try {
         cb();
       } catch (e) {
+        DebugTool.info('Exception: ' + e);
         setTimeout(function () {
           throw e;
         }, 0);
