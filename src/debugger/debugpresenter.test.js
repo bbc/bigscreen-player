711099e244822d72accb6966fb1201318cb4b1d8
Luke Chatburn <luke.chatburn@bbc.co.uk>
2021-08-02 15:26:50 +0100

    Moved more debugger tests

--- src/debugger/debugpresenter.test.js
+++ src/debugger/debugpresenter.test.js
@@ -1,229 +1,227 @@
-require(
-  [
-    'bigscreenplayer/debugger/debugpresenter',
-    'bigscreenplayer/models/mediastate'
-  ],
-  function (DebugPresenter, MediaState) {
-    describe('Debug Presenter', function () {
-      var presenter;
-      var viewMock;
+import DebugPresenter from './debugpresenter'
+import MediaState from '../models/mediastate'
 
-      beforeEach(function () {
-        viewMock = jasmine.createSpyObj('view', ['render']);
-        presenter = DebugPresenter;
-        presenter.init(viewMock);
-      });
+describe('Debug Presenter', function () {
+  var presenter
+  var viewMock
 
-      it('parses static info from an array of chronicle values', function () {
-        presenter.update([{type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1000'}, timestamp: 1518018558259}]);
-        var expectedObject = {
-          static: [
-            {
-              key: 'bitrate',
-              value: '1000'
-            }
-          ],
-          dynamic: [
-          ]
-        };
+  beforeEach(function () {
+    viewMock = {
+      'render': jest.fn()
+    }
+    presenter = DebugPresenter
+    presenter.init(viewMock)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses static info from an array of chronicle values', function () {
+    presenter.update([{type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1000'}, timestamp: 1518018558259}])
+    var expectedObject = {
+      static: [
+        {
+          key: 'bitrate',
+          value: '1000'
+        }
+      ],
+      dynamic: [
+      ]
+    }
 
-      it('converts any static field Date object into human readable time string', function () {
-        var testDate = new Date(1518018558259);
-        presenter.update([{type: 'keyvalue', keyvalue: {key: 'anything', value: testDate}, timestamp: 1518018558259}]);
-        var expectedObject = {
-          static: [
-            {
-              key: 'anything',
-              value: '15:49:18'
-            }
-          ],
-          dynamic: [
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('converts any static field Date object into human readable time string', function () {
+    var testDate = new Date(1518018558259)
+    presenter.update([{type: 'keyvalue', keyvalue: {key: 'anything', value: testDate}, timestamp: 1518018558259}])
+    var expectedObject = {
+      static: [
+        {
+          key: 'anything',
+          value: '15:49:18'
+        }
+      ],
+      dynamic: [
+      ]
+    }
 
-      it('parses dynamic info from an array of chronicle values', function () {
-        presenter.update([{type: 'info', message: 'A string info message', timestamp: 1518018558259}]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Info: A string info message'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses dynamic info from an array of chronicle values', function () {
+    presenter.update([{type: 'info', message: 'A string info message', timestamp: 1518018558259}])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Info: A string info message'
+      ]
+    }
 
-      it('parses multiple dynamic events from an array of chronicle values', function () {
-        presenter.update([
-          {type: 'info', message: 'A string info message', timestamp: 1518018558259},
-          {type: 'info', message: 'Another info message', timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Info: A string info message',
-            '2018-02-07T15:49:18.259Z - Info: Another info message'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses multiple dynamic events from an array of chronicle values', function () {
+    presenter.update([
+      {type: 'info', message: 'A string info message', timestamp: 1518018558259},
+      {type: 'info', message: 'Another info message', timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Info: A string info message',
+        '2018-02-07T15:49:18.259Z - Info: Another info message'
+      ]
+    }
 
-      it('parses error events to simple string representation', function () {
-        presenter.update([
-          {type: 'error',
-            error: {
-              errorId: '1',
-              message: 'An error has occurred'
-            },
-            timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Error: 1 | An error has occurred'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses error events to simple string representation', function () {
+    presenter.update([
+      {type: 'error',
+        error: {
+          errorId: '1',
+          message: 'An error has occurred'
+        },
+        timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Error: 1 | An error has occurred'
+      ]
+    }
 
-      it('parses events to readable representation', function () {
-        presenter.update([
-          {type: 'event', event: {state: MediaState.PLAYING}, timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Event: PLAYING'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses events to readable representation', function () {
+    presenter.update([
+      {type: 'event', event: {state: MediaState.PLAYING}, timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Event: PLAYING'
+      ]
+    }
 
-      it('parses time to formatted string representation', function () {
-        presenter.update([
-          {type: 'time', currentTime: 12.3433, timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Video time: 12.34'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses time to formatted string representation', function () {
+    presenter.update([
+      {type: 'time', currentTime: 12.3433, timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Video time: 12.34'
+      ]
+    }
 
-      it('parses long time to formatted string representation', function () {
-        presenter.update([
-          {type: 'time', currentTime: 788.9999, timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Video time: 789.00'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses long time to formatted string representation', function () {
+    presenter.update([
+      {type: 'time', currentTime: 788.9999, timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Video time: 789.00'
+      ]
+    }
 
-      it('parses apicall to a formatted string representation', function () {
-        presenter.update([
-          {type: 'apicall', calltype: 'Play', timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Api call: Play'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('parses apicall to a formatted string representation', function () {
+    presenter.update([
+      {type: 'apicall', calltype: 'Play', timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Api call: Play'
+      ]
+    }
 
-      it('When the log object does not contain a valid type', function () {
-        presenter.update([
-          {type: 'blah', someobject: {thing: ''}, timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-          ],
-          dynamic: [
-            '2018-02-07T15:49:18.259Z - Unknown log format'
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('When the log object does not contain a valid type', function () {
+    presenter.update([
+      {type: 'blah', someobject: {thing: ''}, timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+      ],
+      dynamic: [
+        '2018-02-07T15:49:18.259Z - Unknown log format'
+      ]
+    }
 
-      it('Only uses the the latest value when updating static fields', function () {
-        presenter.update([
-          {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1000'}, timestamp: 1518018558259},
-          {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '2000'}, timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-            {
-              key: 'bitrate',
-              value: '2000'
-            }
-          ],
-          dynamic: [
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
+  it('Only uses the the latest value when updating static fields', function () {
+    presenter.update([
+      {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1000'}, timestamp: 1518018558259},
+      {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '2000'}, timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+        {
+          key: 'bitrate',
+          value: '2000'
+        }
+      ],
+      dynamic: [
+      ]
+    }
 
-      it('Only uses the the latest value when updating static fields with multiple fields', function () {
-        presenter.update([
-          {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1000'}, timestamp: 1518018558259},
-          {type: 'keyvalue', keyvalue: {key: 'duration', value: '12345'}, timestamp: 1518018558259},
-          {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '2000'}, timestamp: 1518018558259},
-          {type: 'keyvalue', keyvalue: {key: 'duration', value: '12346'}, timestamp: 1518018558259},
-          {type: 'keyvalue', keyvalue: {key: 'seekableRangeStart', value: '0'}, timestamp: 1518018558259},
-          {type: 'keyvalue', keyvalue: {key: 'seekableRangeEnd', value: '12346'}, timestamp: 1518018558259}
-        ]);
-        var expectedObject = {
-          static: [
-            {
-              key: 'bitrate',
-              value: '2000'
-            },
-            {
-              key: 'duration',
-              value: '12346'
-            },
-            {
-              key: 'seekable range start',
-              value: '0'
-            },
-            {
-              key: 'seekable range end',
-              value: '12346'
-            }
-          ],
-          dynamic: [
-          ]
-        };
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
 
-        expect(viewMock.render).toHaveBeenCalledWith(expectedObject);
-      });
-    });
-  });
+  it('Only uses the the latest value when updating static fields with multiple fields', function () {
+    presenter.update([
+      {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1000'}, timestamp: 1518018558259},
+      {type: 'keyvalue', keyvalue: {key: 'duration', value: '12345'}, timestamp: 1518018558259},
+      {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '2000'}, timestamp: 1518018558259},
+      {type: 'keyvalue', keyvalue: {key: 'duration', value: '12346'}, timestamp: 1518018558259},
+      {type: 'keyvalue', keyvalue: {key: 'seekableRangeStart', value: '0'}, timestamp: 1518018558259},
+      {type: 'keyvalue', keyvalue: {key: 'seekableRangeEnd', value: '12346'}, timestamp: 1518018558259}
+    ])
+    var expectedObject = {
+      static: [
+        {
+          key: 'bitrate',
+          value: '2000'
+        },
+        {
+          key: 'duration',
+          value: '12346'
+        },
+        {
+          key: 'seekable range start',
+          value: '0'
+        },
+        {
+          key: 'seekable range end',
+          value: '12346'
+        }
+      ],
+      dynamic: [
+      ]
+    }
+
+    expect(viewMock.render).toHaveBeenCalledWith(expectedObject)
+  })
+})
--- src/debugger/debugtool.test.js
+++ src/debugger/debugtool.test.js
@@ -1,59 +1,55 @@
-require(
-  [
-    'bigscreenplayer/debugger/chronicle',
-    'bigscreenplayer/debugger/debugtool'
-  ],
-  function (Chronicle, DebugTool) {
-    describe('Debug Tool, when intercepting keyValue calls,', function () {
-      beforeEach(function () {
-        Chronicle.init();
-        spyOn(window, 'Date').and.callFake(function () {
-          return {getTime: function () { return 1234; }};
-        });
-      });
+import Chronicle from './chronicle'
+import DebugTool from './debugtool'
 
-      afterEach(function () {
-        DebugTool.tearDown();
-        Chronicle.tearDown();
-      });
+describe('Debug Tool, when intercepting keyValue calls,', function () {
+  beforeEach(function () {
+    jest.spyOn(global, 'Date').mockImplementation(function () {
+      return {getTime: function () { return 1234 }}
+    })
+    Chronicle.init()
+  })
 
-      it('should always add entry to chronicle if the key does not match one of the defined static keys', function () {
-        var testObj1 = {key: 'bitrate', value: '1000'};
-        var testObj2 = {key: 'imNotSpecial', value: 'nobodylovesme'};
-        var testObj3 = {key: 'idontmatch', value: 'pleaseaddme'};
+  afterEach(function () {
+    DebugTool.tearDown()
+    Chronicle.tearDown()
+  })
 
-        var expectedArray = [
-          {type: 'keyvalue', keyvalue: testObj1, timestamp: 1234},
-          {type: 'keyvalue', keyvalue: testObj2, timestamp: 1234},
-          {type: 'keyvalue', keyvalue: testObj3, timestamp: 1234}
-        ];
+  it('should always add entry to chronicle if the key does not match one of the defined static keys', function () {
+    var testObj1 = {key: 'bitrate', value: '1000'}
+    var testObj2 = {key: 'imNotSpecial', value: 'nobodylovesme'}
+    var testObj3 = {key: 'idontmatch', value: 'pleaseaddme'}
 
-        DebugTool.keyValue(testObj1);
-        DebugTool.keyValue(testObj2);
-        DebugTool.keyValue(testObj3);
+    var expectedArray = [
+      {type: 'keyvalue', keyvalue: testObj1, timestamp: 1234},
+      {type: 'keyvalue', keyvalue: testObj2, timestamp: 1234},
+      {type: 'keyvalue', keyvalue: testObj3, timestamp: 1234}
+    ]
 
-        var chronicle = Chronicle.retrieve();
+    DebugTool.keyValue(testObj1)
+    DebugTool.keyValue(testObj2)
+    DebugTool.keyValue(testObj3)
 
-        expect(chronicle).toEqual(expectedArray);
-      });
+    var chronicle = Chronicle.retrieve()
 
-      it('overwrites a keyvalue entry to the chronicle if that keyvalue already exists', function () {
-        var testObj = {key: 'akey', value: 'something'};
-        var testObj1 = {key: 'bitrate', value: '1000'};
-        var testObj2 = {key: 'bitrate', value: '1001'};
+    expect(chronicle).toEqual(expectedArray)
+  })
 
-        var expectedArray = [
-          {type: 'keyvalue', keyvalue: {key: 'akey', value: 'something'}, timestamp: 1234},
-          {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1001'}, timestamp: 1234}
-        ];
+  it('overwrites a keyvalue entry to the chronicle if that keyvalue already exists', function () {
+    var testObj = {key: 'akey', value: 'something'}
+    var testObj1 = {key: 'bitrate', value: '1000'}
+    var testObj2 = {key: 'bitrate', value: '1001'}
 
-        DebugTool.keyValue(testObj);
-        DebugTool.keyValue(testObj1);
-        DebugTool.keyValue(testObj2);
+    var expectedArray = [
+      {type: 'keyvalue', keyvalue: {key: 'akey', value: 'something'}, timestamp: 1234},
+      {type: 'keyvalue', keyvalue: {key: 'bitrate', value: '1001'}, timestamp: 1234}
+    ]
 
-        var chronicle = Chronicle.retrieve();
+    DebugTool.keyValue(testObj)
+    DebugTool.keyValue(testObj1)
+    DebugTool.keyValue(testObj2)
 
-        expect(chronicle).toEqual(expectedArray);
-      });
-    });
-  });
+    var chronicle = Chronicle.retrieve()
+
+    expect(chronicle).toEqual(expectedArray)
+  })
+})
