929a5b243ef32e049ea354ea0ff308d68b6f4627
Peter Clay Holden Morris-Hind <peter.morris-hind@bbc.co.uk>
2021-08-03 11:28:13 +0100

    Convert resizer test

--- src/resizer.test.js
+++ src/resizer.test.js
@@ -1,67 +1,66 @@
-require(
-  [
-    'bigscreenplayer/resizer'
-  ],
-  function (Resizer) {
-    'use strict';
+/**
+ * @jest-environment jsdom
+ */
 
-    describe('Resizer', function () {
-      var resizer, element;
-      beforeEach(function () {
-        element = document.createElement('div');
-        element.style.top = '0px';
-        element.style.left = '0px';
-        element.style.width = '1280px';
-        element.style.height = '720px';
-        resizer = Resizer();
-      });
+import Resizer from './resizer'
 
-      describe('resize', function () {
-        it('Resizes and positions the element with the correct values', function () {
-          resizer.resize(element, 10, 20, 3, 4, 5);
+describe('Resizer', function () {
+  var resizer, element;
 
-          expect(element.style.top).toEqual('10px');
-          expect(element.style.left).toEqual('20px');
-          expect(element.style.width).toEqual('3px');
-          expect(element.style.height).toEqual('4px');
-          expect(element.style.zIndex).toEqual('5');
-          expect(element.style.position).toEqual('absolute');
-        });
-      });
+  beforeEach(function () {
+    element = document.createElement('div');
+    element.style.top = '0px';
+    element.style.left = '0px';
+    element.style.width = '1280px';
+    element.style.height = '720px';
+    resizer = Resizer();
+  });
 
-      describe('clear', function () {
-        it('resets the css properties', function () {
-          resizer.resize(element, 1, 2, 3, 4, 5);
-          resizer.clear(element);
+  describe('resize', function () {
+    it('Resizes and positions the element with the correct values', function () {
+      resizer.resize(element, 10, 20, 3, 4, 5);
 
-          expect(element.style.top).toEqual('');
-          expect(element.style.left).toEqual('');
-          expect(element.style.width).toEqual('');
-          expect(element.style.height).toEqual('');
-          expect(element.style.zIndex).toEqual('');
-          expect(element.style.position).toEqual('');
-        });
-      });
-
-      describe('isResized', function () {
-        it('should return false if no call to resize or clear has been made', function () {
-          expect(resizer.isResized()).toBe(false);
-        });
-
-        it('should return true if the last call was to resized', function () {
-          resizer.clear(element);
-          resizer.resize(element, 1, 2, 3, 4, 5);
-
-          expect(resizer.isResized()).toBe(true);
-        });
-
-        it('should return true if the last call was to clear', function () {
-          resizer.resize(element, 1, 2, 3, 4, 5);
-          resizer.clear(element);
-
-          expect(resizer.isResized()).toBe(false);
-        });
-      });
+      expect(element.style.top).toEqual('10px');
+      expect(element.style.left).toEqual('20px');
+      expect(element.style.width).toEqual('3px');
+      expect(element.style.height).toEqual('4px');
+      expect(element.style.zIndex).toEqual('5');
+      expect(element.style.position).toEqual('absolute');
     });
   });
+
+  describe('clear', function () {
+    it('resets the css properties', function () {
+      resizer.resize(element, 1, 2, 3, 4, 5);
+      resizer.clear(element);
+
+      expect(element.style.top).toEqual('');
+      expect(element.style.left).toEqual('');
+      expect(element.style.width).toEqual('');
+      expect(element.style.height).toEqual('');
+      expect(element.style.zIndex).toEqual('');
+      expect(element.style.position).toEqual('');
+    });
+  });
+
+  describe('isResized', function () {
+    it('should return false if no call to resize or clear has been made', function () {
+      expect(resizer.isResized()).toBe(false);
+    });
+
+    it('should return true if the last call was to resized', function () {
+      resizer.clear(element);
+      resizer.resize(element, 1, 2, 3, 4, 5);
+
+      expect(resizer.isResized()).toBe(true);
+    });
+
+    it('should return true if the last call was to clear', function () {
+      resizer.resize(element, 1, 2, 3, 4, 5);
+      resizer.clear(element);
+
+      expect(resizer.isResized()).toBe(false);
+    });
+  });
+});
 
