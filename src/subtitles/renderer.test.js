5993007eaa1f7fb92c81c81e59f5aa3ef3e6359a
Peter Clay Holden Morris-Hind <peter.morris-hind@bbc.co.uk>
2021-08-10 09:57:01 +0100

    Convert Subtitles Renderer Test

--- src/subtitles/renderer.test.js
+++ src/subtitles/renderer.test.js
@@ -1,50 +1,35 @@
-require(
-  [
-    'squire'
-  ],
-  function (Squire) {
-    describe('Renderer', function () {
-      var Renderer;
-      var squire;
-      var transformerMock;
+import Renderer from './renderer'
 
-      beforeEach(function (done) {
-        transformerMock = function () {
-          return {
-            transformXML: function () {
-              return {
-                baseStyle: '',
-                subtitlesForTime: function () {}
-              };
-            }
-          };
-        };
-
-        squire = new Squire();
-        squire.mock({
-          'bigscreenplayer/subtitles/transformer': transformerMock
-        });
-        squire.require(['bigscreenplayer/subtitles/renderer'], function (RendererWithMocks) {
-          Renderer = RendererWithMocks;
-          done();
-        });
-      });
-
-      it('should initialise with a id, xml object, media player', function () {
-        var mockMediaPlayer = jasmine.createSpy();
-        var renderer = Renderer('subtitlesOutputId', '', mockMediaPlayer);
-
-        expect(renderer).toEqual(jasmine.objectContaining({render: jasmine.any(Function), start: jasmine.any(Function), stop: jasmine.any(Function)}));
-      });
-
-      it('should set the output elements display style on initialisation', function () {
-        var mockMediaPlayer = jasmine.createSpy();
-        var renderer = Renderer('subtitlesOutputId', '', mockMediaPlayer);
-        var outputElement = renderer.render();
-
-        expect(outputElement.style.display).toBe('block');
-      });
-    });
+jest.mock('./transformer', () => {
+  return () => {
+    return {
+      transformXML: () => {
+        return {
+          baseStyle: '',
+          subtitlesForTime: () => {}
+        }
+      }
+    }
   }
-);
+})
 
+describe('Renderer', function () {
+  it('should initialise with a id, xml object, media player', function () {
+    var mockMediaPlayer = jest.fn()
+    var renderer = Renderer('subtitlesOutputId', '', mockMediaPlayer)
+
+    expect(renderer).toEqual(expect.objectContaining({
+      render: expect.any(Function),
+      start: expect.any(Function),
+      stop: expect.any(Function)
+    }))
+  })
+
+  it('should set the output elements display style on initialisation', function () {
+    var mockMediaPlayer = jest.fn()
+    var renderer = Renderer('subtitlesOutputId', '', mockMediaPlayer)
+    var outputElement = renderer.render()
+
+    expect(outputElement.style.display).toBe('block')
+  })
+})
