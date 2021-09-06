161c95ad43d7658ff763e1a5453bf1b2ca51382d
Dom Jackson <dom.jackson@bbc.co.uk>
2021-09-06 12:31:28 +0100

    Make subtitle header XML split more general (#237)
    * Make subtitle header XML split more general
    
    * Fix IMSC test now regex matches

--- script-test/subtitles/imscsubtitles.js
+++ script-test/subtitles/imscsubtitles.js
@@ -283,7 +283,7 @@ require(
           loadUrlStubResponseText = '<?xml version="1.0" encoding="utf-8" extra property="something"?><tt xmlns="http://www.w3.org/ns/ttml"></tt>';
           subtitles = ImscSubtitles(mediaPlayer, true, mockParentElement, mockMediaSources, {});
 
-          expect(imscMock.fromXML).toHaveBeenCalledWith(loadUrlStubResponseText);
+          expect(imscMock.fromXML).toHaveBeenCalledWith('<tt xmlns="http://www.w3.org/ns/ttml"></tt>');
         });
 
         it('fires tranformError plugin if IMSC throws an exception when parsing', function () {
