22af480e30929dd1ac3cd2f8222c248eed2ce151
Joel Keers <joel.keers@bbc.co.uk>
2021-09-07 12:01:33 +0100

    only use module output

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,13 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import nodePolyfills from 'rollup-plugin-polyfill-node'
-import babel from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
-import pkg from './package.json'
-import { terser } from 'rollup-plugin-terser'
-import { visualizer } from 'rollup-plugin-visualizer'
 
-export default [{
-  input: 'src/main.js',
-  output: {
-    inlineDynamicImports: true,
-    name: 'bsp',
-    file: pkg.browser,
-    sourcemap: true,
-    format: 'umd'
-  },
-  plugins: [
-    resolve({ browser: true, preferBuiltins: false }),
-    commonjs(),
-    json(),
-    nodePolyfills(),
-    visualizer(),
-    babel({ babelHelpers: 'bundled', presets: ['@babel/preset-env'] }),
-    terser({format: {comments: 'all'}})
-  ]
-},
+export default
 {
   input: 'src/main.js',
   external: ['dashjs', 'smp-imsc'],
   output: [
     { dir: 'dist/esm', format: 'es' }
   ],
-  plugins: [    
+  plugins: [
     json()
   ]
-}]
+}
