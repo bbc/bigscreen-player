c017915921162c414504365ffd96373d72cc259e
Joel Keers <joel.keers@bbc.co.uk>
2021-10-05 10:25:12 +0100

    force imscJS to smp fork

--- src/subtitles/imscsubtitles.js
+++ src/subtitles/imscsubtitles.js
@@ -1,4 +1,4 @@
-import { fromXML, generateISD, renderHTML } from 'smp-imsc'
+import { fromXML, generateISD, renderHTML } from 'imsc'
 import DOMHelpers from '../domhelpers'
 import DebugTool from '../debugger/debugtool'
 import Plugins from '../plugins'
