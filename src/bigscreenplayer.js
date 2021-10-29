7a28870c436225ed763f968f007312fb8d75bf3e
Dom Jackson <dom.jackson@bbc.co.uk>
2021-10-29 13:24:27 +0100

    Move typedefs into seperate file

--- src/bigscreenplayer.js
+++ src/bigscreenplayer.js
@@ -17,6 +17,7 @@ import Version from './version'
 import Resizer from './resizer'
 import ReadyHelper from './readyhelper'
 import Subtitles from './subtitles/subtitles'
+import './typedefs'
 
 function BigscreenPlayer () {
   let stateChangeCallbacks = []
@@ -176,30 +177,6 @@ function BigscreenPlayer () {
   }
 
   return /** @alias module:bigscreenplayer/bigscreenplayer */{
-    /**
-     * Data required for playback
-     * @typedef {Object} BigscreenPlayerData
-     * @property {Object} media
-     * @property {String} media.type - source type e.g 'application/dash+xml'
-     * @property {String} media.mimeType - mimeType e.g 'video/mp4'
-     * @property {String} media.kind - 'video' or 'audio'
-     * @property {String} media.captionsUrl - 'Location for a captions file'
-     * @property {MediaUrl[]} media.urls - Media urls to use
-     * @property {Date} serverDate - Date object with server time offset
-     */
-
-    /**
-      * @typedef {Object} MediaUrl
-      * @property {String} url - media endpoint
-      * @property {String} cdn - identifier for the endpoint
-      */
-
-    /**
-     *
-     * @typedef {object} InitCallbacks
-     * @property {function} [callbacks.onSuccess] - Called after Bigscreen Player is initialised
-     * @property {function} [callbacks.onError] - Called when an error occurs during initialisation
-     */
 
     /**
      * Call first to initialise bigscreen player for playback.
---
+++ src/typedefs.js
@@ -0,0 +1,24 @@
+/**
+ * Data required for playback
+ * @typedef {Object} BigscreenPlayerData
+ * @property {Object} media
+ * @property {String} media.type - source type e.g 'application/dash+xml'
+ * @property {String} media.mimeType - mimeType e.g 'video/mp4'
+ * @property {String} media.kind - 'video' or 'audio'
+ * @property {String} media.captionsUrl - 'Location for a captions file'
+ * @property {MediaUrl[]} media.urls - Media urls to use
+ * @property {Date} serverDate - Date object with server time offset
+ */
+
+/**
+ * @typedef {Object} MediaUrl
+ * @property {String} url - media endpoint
+ * @property {String} cdn - identifier for the endpoint
+ */
+
+/**
+ *
+ * @typedef {object} InitCallbacks
+ * @property {function} [callbacks.onSuccess] - Called after Bigscreen Player is initialised
+ * @property {function} [callbacks.onError] - Called when an error occurs during initialisation
+ */
