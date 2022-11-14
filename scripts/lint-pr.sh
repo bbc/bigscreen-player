ac7a3aa256862d321fc5e80dd32b46720b0305f4
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2022-11-14 16:16:31 +0000

    fix(action): manually exit early

--- scripts/lint-pr.sh
+++ scripts/lint-pr.sh
@@ -5,8 +5,13 @@ PR_HEAD_SHA="${PR_HEAD_SHA}"
 
 changed_files="$(git diff --name-only --diff-filter=ACMR ${PR_BASE_SHA} ${PR_HEAD_SHA})"
 
+# Manually check the `git diff` succeeded
+[ "${?}" -ne 0 ] && exit 1
+
+# Exit early if no files were changed
 [ -z "${changed_files}" ] && exit 0
 
+# Lint changed files
 printf "%s\n" "${changed_files}" \
     | sed 's| |\\ |g' \
     | xargs ./node_modules/.bin/eslint --quiet
