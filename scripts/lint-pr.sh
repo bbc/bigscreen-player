abedba3d8538caf88624db3ec82a700132bf3233
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2022-11-14 16:10:36 +0000

    fix: fail fast linting pr

--- scripts/lint-pr.sh
+++ scripts/lint-pr.sh
@@ -3,16 +3,14 @@
 PR_BASE_SHA="${PR_BASE_SHA}"
 PR_HEAD_SHA="${PR_HEAD_SHA}"
 
-changed_files="$(
-    git diff --name-only --diff-filter=ACMR ${PR_BASE_SHA} ${PR_HEAD_SHA} \
-    | sed 's| |\\ |g' \
-    | xargs \
-)"
+changed_files="$(git diff --name-only --diff-filter=ACMR ${PR_BASE_SHA} ${PR_HEAD_SHA})"
 
 [ -z "${changed_files}" ] && exit 0
 
-printf "%s\n" "${changed_files}" | xargs ./node_modules/.bin/eslint --quiet
+printf "%s\n" "${changed_files}" \
+    | sed 's| |\\ |g' \
+    | xargs ./node_modules/.bin/eslint --quiet
 
-[ "${?}" -ne 1 ] exit 1
+[ "${?}" -ne 0 ] && exit 1
 
 exit 0
