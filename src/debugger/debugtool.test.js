e1c46afc4798688c98890d3f79f3fc8d7e9753f7
Dom Jackson <dom.jackson@bbc.co.uk>
2023-03-03 16:44:59 +0000

    Added some basic unit testing for get and setters

--- src/debugger/debugtool.test.js
+++ src/debugger/debugtool.test.js
@@ -1,6 +1,26 @@
 import Chronicle from './chronicle'
 import DebugTool from './debugtool'
 
+describe('Debug Tool, when configuring log levels,', () => {
+  it('should return an object of log levels', () => {
+    expect(DebugTool.logLevels && typeof DebugTool.logLevels === 'object').toBe(true)
+  })
+
+  it('should default to LOG_LEVELS.INFO', () => {
+    expect(DebugTool.getLogLevel()).toEqual(DebugTool.logLevels.INFO)
+  })
+
+  it('does not override log level when setLogLevel argument is undefined', () => {
+    DebugTool.setLogLevel()
+    expect(DebugTool.getLogLevel()).toEqual(DebugTool.logLevels.INFO)
+  })
+
+  it('can set a new log level', () => {
+    DebugTool.setLogLevel(DebugTool.logLevels.ERROR)
+    expect(DebugTool.getLogLevel()).toEqual(DebugTool.logLevels.ERROR)
+  })
+})
+
 describe('Debug Tool, when intercepting keyValue calls,', () => {
   beforeEach(() => {
     jest.spyOn(global, 'Date').mockImplementation(() => {
