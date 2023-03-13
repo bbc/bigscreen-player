ee015df287ec5f09eee5b27170137b12bf373512
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2023-03-13 12:26:49 +0000

    style: lint+format subtitles

--- src/subtitles/subtitles.js
+++ src/subtitles/subtitles.js
@@ -1,92 +1,92 @@
-import Plugins from '../plugins'
+import Plugins from "../plugins"
 
-function Subtitles (mediaPlayer, autoStart, playbackElement, defaultStyleOpts, mediaSources, callback) {
+function Subtitles(mediaPlayer, autoStart, playbackElement, defaultStyleOpts, mediaSources, callback) {
   const liveSubtitles = !!mediaSources.currentSubtitlesSegmentLength()
-  const useLegacySubs = window.bigscreenPlayer && window.bigscreenPlayer.overrides && window.bigscreenPlayer.overrides.legacySubtitles || false
+  const useLegacySubs = window.bigscreenPlayer?.overrides?.legacySubtitles ?? false
 
   let subtitlesEnabled = autoStart
   let subtitlesContainer
 
   if (useLegacySubs) {
-    import('./legacysubtitles.js').then(({ default: LegacySubtitles }) => {
-      subtitlesContainer = LegacySubtitles(mediaPlayer, autoStart, playbackElement, mediaSources, defaultStyleOpts)
-      callback(subtitlesEnabled)
-    }).catch(() => {
-      Plugins.interface.onSubtitlesDynamicLoadError()
-    })
+    import("./legacysubtitles.js")
+      .then(({ default: LegacySubtitles }) => {
+        subtitlesContainer = LegacySubtitles(mediaPlayer, autoStart, playbackElement, mediaSources, defaultStyleOpts)
+        callback(subtitlesEnabled)
+      })
+      .catch(() => {
+        Plugins.interface.onSubtitlesDynamicLoadError()
+      })
   } else {
-    import('./imscsubtitles.js').then(({ default: IMSCSubtitles }) => {
-      subtitlesContainer = IMSCSubtitles(mediaPlayer, autoStart, playbackElement, mediaSources, defaultStyleOpts)
-      callback(subtitlesEnabled)
-    }).catch(() => {
-      Plugins.interface.onSubtitlesDynamicLoadError()
-    })
+    import("./imscsubtitles.js")
+      .then(({ default: IMSCSubtitles }) => {
+        subtitlesContainer = IMSCSubtitles(mediaPlayer, autoStart, playbackElement, mediaSources, defaultStyleOpts)
+        callback(subtitlesEnabled)
+      })
+      .catch(() => {
+        Plugins.interface.onSubtitlesDynamicLoadError()
+      })
   }
 
-  function enable () {
+  function enable() {
     subtitlesEnabled = true
   }
 
-  function disable () {
+  function disable() {
     subtitlesEnabled = false
   }
 
-  function show () {
+  function show() {
     if (available() && enabled()) {
-      subtitlesContainer && subtitlesContainer.start()
+      subtitlesContainer?.start()
     }
   }
 
-  function hide () {
+  function hide() {
     if (available()) {
-      subtitlesContainer && subtitlesContainer.stop()
+      subtitlesContainer?.stop()
     }
   }
 
-  function enabled () {
+  function enabled() {
     return subtitlesEnabled
   }
 
-  function available () {
-    if (liveSubtitles && (window.bigscreenPlayer.overrides && window.bigscreenPlayer.overrides.legacySubtitles)) {
-      return false
-    } else {
-      return !!mediaSources.currentSubtitlesSource()
-    }
+  function available() {
+    return liveSubtitles && useLegacySubs ? false : !!mediaSources.currentSubtitlesSource()
   }
 
-  function setPosition (position) {
-    subtitlesContainer && subtitlesContainer.updatePosition(position)
+  function setPosition(position) {
+    subtitlesContainer?.updatePosition(position)
   }
 
-  function customise (styleOpts) {
-    subtitlesContainer && subtitlesContainer.customise(styleOpts, subtitlesEnabled)
+  function customise(styleOpts) {
+    subtitlesContainer?.customise(styleOpts, subtitlesEnabled)
   }
 
-  function renderExample (exampleXmlString, styleOpts, safePosition) {
-    subtitlesContainer && subtitlesContainer.renderExample(exampleXmlString, styleOpts, safePosition)
+  function renderExample(exampleXmlString, styleOpts, safePosition) {
+    subtitlesContainer?.renderExample(exampleXmlString, styleOpts, safePosition)
   }
 
-  function clearExample () {
-    subtitlesContainer && subtitlesContainer.clearExample()
+  function clearExample() {
+    subtitlesContainer?.clearExample()
   }
 
-  function tearDown () {
-    subtitlesContainer && subtitlesContainer.tearDown()
+  function tearDown() {
+    subtitlesContainer?.tearDown()
   }
 
   return {
-    enable: enable,
-    disable: disable,
-    show: show,
-    hide: hide,
-    enabled: enabled,
-    available: available,
-    setPosition: setPosition,
-    customise: customise,
-    renderExample: renderExample,
-    clearExample: clearExample,
-    tearDown: tearDown
+    enable,
+    disable,
+    show,
+    hide,
+    enabled,
+    available,
+    setPosition,
+    customise,
+    renderExample,
+    clearExample,
+    tearDown,
   }
 }
 
