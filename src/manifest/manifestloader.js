53de3a44bf32302e82948b74a24303442e1b50ef
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2023-03-16 15:25:46 +0000

    feat: parse presentation time offset off a static manifest

--- src/manifest/manifestloader.js
+++ src/manifest/manifestloader.js
@@ -1,93 +1,84 @@
-import ManifestParser from './manifestparser'
-import TransferFormats from '../models/transferformats'
-import LoadUrl from '../utils/loadurl'
+import ManifestParser from "./manifestparser"
+import TransferFormats from "../models/transferformats"
+import LoadUrl from "../utils/loadurl"
 
-function retrieveDashManifest (url, dateWithOffset, callbacks) {
-  LoadUrl(
-    url,
-    {
-      method: 'GET',
-      headers: {},
-      timeout: 10000,
-      onLoad: (responseXML, _responseText, _status) => {
-        try {
-          if (responseXML) {
-            callbacks.onSuccess({
-              transferFormat: TransferFormats.DASH,
-              time: ManifestParser.parse(responseXML, 'mpd', dateWithOffset)
-            })
-          } else {
-            callbacks.onError('Unable to retrieve DASH XML response')
-          }
-        } catch (ex) {
-          callbacks.onError('Unable to retrieve DASH XML response')
-        }
-      },
-      onError: () => {
-        callbacks.onError('Network error: Unable to retrieve DASH manifest')
-      }
-    }
-  )
-}
-
-function retrieveHLSManifest (url, dateWithOffset, callbacks) {
-  LoadUrl(
-    url,
-    {
-      method: 'GET',
-      headers: {},
-      timeout: 10000,
-      onLoad: (_responseXML, responseText) => {
-        let streamUrl
-
-        if (responseText) {
-          streamUrl = getStreamUrl(responseText)
-        }
-        if (streamUrl) {
-          if (!/^http/.test(streamUrl)) {
-            const parts = url.split('/')
-
-            parts.pop()
-            parts.push(streamUrl)
-            streamUrl = parts.join('/')
-          }
-          loadLivePlaylist(streamUrl, dateWithOffset, callbacks)
-        } else {
-          callbacks.onError('Unable to retrieve HLS master playlist')
-        }
-      },
-      onError: () => {
-        callbacks.onError('Network error: Unable to retrieve HLS master playlist')
-      }
-    }
-  )
-}
-
-function loadLivePlaylist (url, dateWithOffset, callbacks) {
-  LoadUrl(
-    url,
-    {
-      method: 'GET',
-      headers: {},
-      timeout: 10000,
-      onLoad: (_responseXML, responseText) => {
-        if (responseText) {
+function retrieveDashManifest(url, initialWallclockTime, callbacks) {
+  LoadUrl(url, {
+    method: "GET",
+    headers: {},
+    timeout: 10000,
+    onLoad: (responseXML, _responseText, _status) => {
+      try {
+        if (responseXML) {
           callbacks.onSuccess({
-            transferFormat: TransferFormats.HLS,
-            time: ManifestParser.parse(responseText, 'm3u8', dateWithOffset)
+            transferFormat: TransferFormats.DASH,
+            time: ManifestParser.parse(responseXML, { initialWallclockTime, type: "mpd" }),
           })
         } else {
-          callbacks.onError('Unable to retrieve HLS live playlist')
+          callbacks.onError("Unable to retrieve DASH XML response")
         }
-      },
-      onError: () => {
-        callbacks.onError('Network error: Unable to retrieve HLS live playlist')
+      } catch {
+        callbacks.onError("Unable to retrieve DASH XML response")
       }
-    }
-  )
+    },
+    onError: () => {
+      callbacks.onError("Network error: Unable to retrieve DASH manifest")
+    },
+  })
 }
 
-function getStreamUrl (data) {
+function retrieveHLSManifest(url, initialWallclockTime, callbacks) {
+  LoadUrl(url, {
+    method: "GET",
+    headers: {},
+    timeout: 10000,
+    onLoad: (_responseXML, responseText) => {
+      let streamUrl
+
+      if (responseText) {
+        streamUrl = getStreamUrl(responseText)
+      }
+      if (streamUrl) {
+        if (streamUrl.indexOf("http") !== 0) {
+          const parts = url.split("/")
+
+          parts.pop()
+          parts.push(streamUrl)
+          streamUrl = parts.join("/")
+        }
+        loadLivePlaylist(streamUrl, initialWallclockTime, callbacks)
+      } else {
+        callbacks.onError("Unable to retrieve HLS master playlist")
+      }
+    },
+    onError: () => {
+      callbacks.onError("Network error: Unable to retrieve HLS master playlist")
+    },
+  })
+}
+
+function loadLivePlaylist(url, initialWallclockTime, callbacks) {
+  LoadUrl(url, {
+    method: "GET",
+    headers: {},
+    timeout: 10000,
+    onLoad: (_responseXML, responseText) => {
+      if (responseText) {
+        callbacks.onSuccess({
+          transferFormat: TransferFormats.HLS,
+          time: ManifestParser.parse(responseText, { initialWallclockTime, type: "m3u8" }),
+        })
+      } else {
+        callbacks.onError("Unable to retrieve HLS live playlist")
+      }
+    },
+    onError: () => {
+      callbacks.onError("Network error: Unable to retrieve HLS live playlist")
+    },
+  })
+}
+
+function getStreamUrl(data) {
   const match = /#EXT-X-STREAM-INF:.*[\n\r]+(.*)[\n\r]?/.exec(data)
 
   if (match) {
@@ -96,13 +87,13 @@ function getStreamUrl (data) {
 }
 
 export default {
-  load: (mediaUrl, serverDate, callbacks) => {
+  load: (mediaUrl, initialWallclockTime, callbacks) => {
     if (/\.m3u8($|\?.*$)/.test(mediaUrl)) {
-      retrieveHLSManifest(mediaUrl, serverDate, callbacks)
+      retrieveHLSManifest(mediaUrl, initialWallclockTime, callbacks)
     } else if (/\.mpd($|\?.*$)/.test(mediaUrl)) {
-      retrieveDashManifest(mediaUrl, serverDate, callbacks)
+      retrieveDashManifest(mediaUrl, initialWallclockTime, callbacks)
     } else {
-      callbacks.onError('Invalid media url')
+      callbacks.onError("Invalid media url")
     }
-  }
+  },
 }
--- src/manifest/manifestparser.js
+++ src/manifest/manifestparser.js
@@ -1,44 +1,92 @@
-import TimeUtils from './../utils/timeutils'
-import DebugTool from '../debugger/debugtool'
-import Plugins from '../plugins'
-import pluginenums from '../pluginenums'
+import TimeUtils from "./../utils/timeutils"
+import DebugTool from "../debugger/debugtool"
+import WindowTypes from "../models/windowtypes"
+import Plugins from "../plugins"
+import PluginEnums from "../pluginenums"
 
-function parseMPD (manifest, dateWithOffset) {
-  try {
-    const mpd = manifest.getElementsByTagName('MPD')[0]
-    const availabilityStartTime = Date.parse(mpd.getAttribute('availabilityStartTime'))
-    const tsbdAttr = mpd.getAttribute('timeShiftBufferDepth')
-    const timeShiftBufferDepth = tsbdAttr && TimeUtils.durationToSeconds(tsbdAttr)
+function getSegmentTemplate(mpd) {
+  // Can be either audio or video data.
+  // It doesn't matter as we use the factor of x/timescale. This is the same for both.
+  const segmentTemplate = mpd.querySelector("SegmentTemplate")
 
-    // Getting zeroth SegmentTemplate may grab either audio or video
-    // data. This shouldn't matter as we only use the factor of
-    // duration/timescale, which is the same for both.
-    const segmentTemplate = manifest.getElementsByTagName('SegmentTemplate')[0]
-    const timescale = parseFloat(segmentTemplate.getAttribute('timescale'))
-    const duration = parseFloat(segmentTemplate.getAttribute('duration'))
-    const oneSegment = 1000 * duration / timescale
-
-    if (availabilityStartTime && oneSegment) {
-      const windowEndTime = dateWithOffset - (timeShiftBufferDepth ? availabilityStartTime : 0) - oneSegment
-      const windowStartTime = timeShiftBufferDepth ? windowEndTime - (timeShiftBufferDepth * 1000) : availabilityStartTime
-      const timeCorrection = timeShiftBufferDepth ? windowStartTime / 1000 : 0
-
-      return {
-        windowStartTime: windowStartTime,
-        windowEndTime: windowEndTime,
-        correction: timeCorrection
-      }
-    } else {
-      throw new Error('manifest-dash-attributes-parse-error')
-    }
-  } catch (e) {
-    const error = new Error(e.message || 'manifest-dash-parse-error')
-    error.code = pluginenums.ERROR_CODES.MANIFEST_PARSE
-    throw error
+  return {
+    duration: parseFloat(segmentTemplate.getAttribute("duration")),
+    timescale: parseFloat(segmentTemplate.getAttribute("timescale")),
+    presentationTimeOffset: parseFloat(segmentTemplate.getAttribute("presentationTimeOffset")),
   }
 }
 
-function parseM3U8 (manifest) {
+function parseStaticMPD(mpd) {
+  const { presentationTimeOffset, timescale } = getSegmentTemplate(mpd)
+
+  return {
+    presentationTimeOffsetSeconds: presentationTimeOffset / timescale,
+    windowStartTime: NaN,
+    windowEndTime: NaN,
+  }
+}
+
+function parseSlidingMPD(mpd, initialWallclockTime) {
+  const { duration, timescale } = getSegmentTemplate(mpd)
+  const availabilityStartTime = mpd.getAttribute("availabilityStartTime")
+  const segmentLengthMillis = (1000 * duration) / timescale
+
+  if (!availabilityStartTime || !segmentLengthMillis) {
+    throw new Error("manifest-dash-attributes-parse-error")
+  }
+
+  const timeShiftBufferDepthMillis = 1000 * TimeUtils.durationToSeconds(mpd.getAttribute("timeShiftBufferDepth"))
+  const windowEndTime = initialWallclockTime - Date.parse(availabilityStartTime) - segmentLengthMillis
+  const windowStartTime = windowEndTime - timeShiftBufferDepthMillis
+
+  return {
+    windowStartTime,
+    windowEndTime,
+    presentationTimeOffsetSeconds: NaN,
+  }
+}
+
+function parseGrowingMPD(mpd, initialWallclockTime) {
+  const { duration, timescale } = getSegmentTemplate(mpd)
+  const availabilityStartTime = mpd.getAttribute("availabilityStartTime")
+  const segmentLengthMillis = (1000 * duration) / timescale
+
+  if (!availabilityStartTime || !segmentLengthMillis) {
+    throw new Error("manifest-dash-attributes-parse-error")
+  }
+
+  return {
+    windowStartTime: Date.parse(availabilityStartTime),
+    windowEndTime: initialWallclockTime - segmentLengthMillis,
+    presentationTimeOffsetSeconds: NaN,
+  }
+}
+
+const parsingStrategyByWindowType = {
+  [WindowTypes.GROWING]: parseGrowingMPD,
+  [WindowTypes.SLIDING]: parseSlidingMPD,
+  [WindowTypes.STATIC]: parseStaticMPD,
+}
+
+function parseMPD(manifest, { windowType, initialWallclockTime } = {}) {
+  try {
+    const mpd = manifest.querySelectorAll("MPD")[0]
+
+    const parse = parsingStrategyByWindowType[windowType]
+
+    if (parse == null) {
+      throw new Error(`Could not find a DASH parsing strategy for window type ${windowType}`)
+    }
+
+    return parse(mpd, initialWallclockTime)
+  } catch (error) {
+    const errorWithCode = new Error(error.message ?? "manifest-dash-parse-error")
+    errorWithCode.code = PluginEnums.ERROR_CODES.MANIFEST_PARSE
+    throw errorWithCode
+  }
+}
+
+function parseM3U8(manifest) {
   try {
     const windowStartTime = getM3U8ProgramDateTime(manifest)
     const duration = getM3U8WindowSizeInSeconds(manifest)
@@ -47,20 +95,21 @@ function parseM3U8 (manifest) {
       const windowEndTime = windowStartTime + duration * 1000
 
       return {
-        windowStartTime: windowStartTime,
-        windowEndTime: windowEndTime
+        windowStartTime,
+        windowEndTime,
+        presentationTimeOffsetSeconds: NaN,
       }
-    } else {
-      throw new Error('manifest-hls-attributes-parse-error')
     }
-  } catch (e) {
-    const error = new Error(e.message || 'manifest-hls-parse-error')
-    error.code = pluginenums.ERROR_CODES.MANIFEST_PARSE
-    throw error
+
+    throw new Error("manifest-hls-attributes-parse-error")
+  } catch (error) {
+    const errorWithCode = new Error(error.message || "manifest-hls-parse-error")
+    errorWithCode.code = PluginEnums.ERROR_CODES.MANIFEST_PARSE
+    throw errorWithCode
   }
 }
 
-function getM3U8ProgramDateTime (data) {
+function getM3U8ProgramDateTime(data) {
   const match = /^#EXT-X-PROGRAM-DATE-TIME:(.*)$/m.exec(data)
 
   if (match) {
@@ -72,39 +121,38 @@ function getM3U8ProgramDateTime (data) {
   }
 }
 
-function getM3U8WindowSizeInSeconds (data) {
+function getM3U8WindowSizeInSeconds(data) {
   const regex = /#EXTINF:(\d+(?:\.\d+)?)/g
   let matches = regex.exec(data)
   let result = 0
 
   while (matches) {
-    result += (+matches[1])
+    result += +matches[1]
     matches = regex.exec(data)
   }
 
   return Math.floor(result)
 }
 
-function parse (manifest, type, dateWithOffset) {
-  const fallback = {
-    windowStartTime: null,
-    windowEndTime: null,
-    correction: 0
-  }
-
+function parse(manifest, { type, windowType, initialWallclockTime } = {}) {
   try {
-    if (type === 'mpd') {
-      return parseMPD(manifest, dateWithOffset)
-    } else if (type === 'm3u8') {
+    if (type === "mpd") {
+      return parseMPD(manifest, { windowType, initialWallclockTime })
+    } else if (type === "m3u8") {
       return parseM3U8(manifest)
     }
-  } catch (e) {
-    DebugTool.info('Manifest Parse Error: ' + e.code + ' ' + e.message)
-    Plugins.interface.onManifestParseError({ code: e.code, message: e.message })
-    return fallback
+  } catch (error) {
+    DebugTool.info(`Manifest Parse Error: ${error.code} ${error.message}`)
+    Plugins.interface.onManifestParseError({ code: error.code, message: error.message })
+
+    return {
+      windowStartTime: NaN,
+      windowEndTime: NaN,
+      presentationTimeOffsetSeconds: NaN,
+    }
   }
 }
 
 export default {
-  parse: parse
+  parse,
 }
