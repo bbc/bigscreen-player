772b2d4a2c1ebccb47257b72fa7c789405f378fa
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2023-06-16 15:51:41 +0100

    docs: reference source

--- src/testutils/geterror.js
+++ src/testutils/geterror.js
@@ -1,6 +1,8 @@
 class NoErrorThrownError extends Error {}
 
 /**
+ * @see {@link https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/no-conditional-expect.md#how-to-catch-a-thrown-error-for-testing-without-violating-this-rule} [2023-06-16]
+ *
  * @param {() => unknown} call
  * @returns {Promise<Error>}
  */
