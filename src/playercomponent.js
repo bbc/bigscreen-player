9a7802816fb13d0e783a53f5206427e2ee2d1b22
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2023-06-28 16:36:36 +0100

    feat: use signal-based triggers

--- src/playercomponent.js
+++ src/playercomponent.js
@@ -1,3 +1,4 @@
+import setupAllTriggers from "./debugger/triggers"
 import MediaState from "./models/mediastate"
 import WindowTypes from "./models/windowtypes"
 import PluginData from "./plugindata"
@@ -29,6 +30,23 @@ function PlayerComponent(
   let mediaMetaData
   let fatalErrorTimeout
   let fatalError
+
+  let teardownTriggers
+
+  if (bigscreenPlayerData.signal) {
+    teardownTriggers = setupAllTriggers(
+      bigscreenPlayerData.signal,
+      [
+        "triggerfailover",
+        () =>
+          attemptCdnFailover({
+            code: PluginEnums.ERROR_CODES.BUFFERING_TIMEOUT,
+            message: PluginEnums.ERROR_MESSAGES.BUFFERING_TIMEOUT,
+          }),
+      ],
+      ["triggerreload", ({ time }) => reloadMediaElement(time)]
+    )
+  }
 
   StrategyPicker()
     .then((strategy) => {
@@ -387,6 +405,11 @@ function PlayerComponent(
     mediaMetaData = undefined
     fatalErrorTimeout = undefined
     fatalError = undefined
+
+    if (typeof teardownTriggers === "function") {
+      teardownTriggers()
+      teardownTriggers = undefined
+    }
   }
 
   return {
