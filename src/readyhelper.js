718a492c886f046c5638124c958b238d5bef3a61
Peter Clay Holden Morris-Hind <peter.morris-hind@bbc.co.uk>
2024-01-04 08:38:06 +0000

    Linting on ReadyHelper

--- src/readyhelper.js
+++ src/readyhelper.js
@@ -28,20 +28,14 @@ function ReadyHelper(initialPlaybackTime, windowType, liveSupport, callback) {
   function isValidTime(evtData) {
     const isStatic = windowType === WindowTypes.STATIC
 
-    if (isStatic) {
-      return validateStaticTime(evtData.currentTime)
-    } else {
-      return validateLiveTime(evtData.currentTime, evtData.seekableRange)
-    }
+    return isStatic
+      ? validateStaticTime(evtData.currentTime)
+      : validateLiveTime(evtData.currentTime, evtData.seekableRange)
   }
 
   function validateStaticTime(currentTime) {
     if (currentTime !== undefined) {
-      if (initialPlaybackTime) {
-        return currentTime > 0
-      } else {
-        return currentTime >= 0
-      }
+      return initialPlaybackTime ? currentTime > 0 : currentTime >= 0
     }
     return false
   }
@@ -51,25 +45,15 @@ function ReadyHelper(initialPlaybackTime, windowType, liveSupport, callback) {
       return currentTime >= 0
     }
 
-    if (!isValidSeekableRange(seekableRange)) {
-      return false
-    }
-
-    return true
+    return isValidSeekableRange(seekableRange)
   }
 
   function isValidSeekableRange(seekableRange) {
-    if (seekableRange) {
-      if (seekableRange.start === 0 && seekableRange.end === 0) {
-        return false
-      }
-      return true
-    }
-    return false
+    return seekableRange ? !(seekableRange.start === 0 && seekableRange.end === 0) : false
   }
 
   return {
-    callbackWhenReady: callbackWhenReady,
+    callbackWhenReady,
   }
 }
 
