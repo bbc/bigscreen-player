703d56b85d0e00ff1162e340901c418759a33847
Peter Clay Holden Morris-Hind <peter.morris-hind@bbc.co.uk>
2024-01-09 13:41:45 +0000

    Remove Terser - Unused

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 import json from "@rollup/plugin-json"
 import typescript from "@rollup/plugin-typescript"
-import terser from "@rollup/plugin-terser"
 
 export default {
   input: "src/main.js",
   external: ["dashjs", "smp-imsc"],
   output: [{ dir: "dist/esm", format: "es" }],
-  plugins: [json(), typescript(), terser()],
+  plugins: [json(), typescript()],
 }
