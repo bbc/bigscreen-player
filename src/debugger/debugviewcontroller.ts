53de0c1f7da0b03af16d4636fc80597d3fe16f48
Eirik Jo Björnerstedt <eirik.bjornerstedt@bbc.co.uk>
2024-03-08 10:31:11 +0000

    fix: replace nbsp with regular space

--- src/debugger/debugviewcontroller.ts
+++ src/debugger/debugviewcontroller.ts
@@ -284,7 +284,7 @@ class DebugViewController {
       return `${qualityIndex} (${bitrate} kbps)`
     }
 
-    return data.join(",&nbsp;")
+    return data.join(", ")
   }
 
   private render(): void {
