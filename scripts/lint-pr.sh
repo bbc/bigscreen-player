ac6c5bf8cd95b3dfee26c42f7c2d6770835e151c
Eirik Jo Björnerstedt <44924723+eirikbjornr@users.noreply.github.com>
2024-03-18 11:12:39 +0000

    chore: use type information for better linting (#328)
    * feat: use flat config
    
    * feat: add jest
    
    * feat: add legacy configs
    
    * style: consistent prop quoting
    
    * chore: remove unnecessary default case
    
    * chore: remove test
    
    * chore: clean up ignore file
    
    * chore: fix naming convention
    
    * chore: fix tsconfig
    
    * style: format scripts
    
    * chore: ignore node modules

--- scripts/lint-pr.sh
+++ scripts/lint-pr.sh
@@ -6,16 +6,23 @@ PR_HEAD_SHA="${PR_HEAD_SHA}"
 changed_files="$(git diff --name-only --diff-filter=ACMR ${PR_BASE_SHA} ${PR_HEAD_SHA})"
 
 # Manually check the `git diff` succeeded
-[ "${?}" -ne 0 ] && exit 1
+if [ "${?}" -ne 0 ]; then
+  exit 1
+fi
 
 # Exit early if no files were changed
-[ -z "${changed_files}" ] && exit 0
+if [ -z "${changed_files}" ]; then
+  exit 0
+fi
 
 # Lint changed files
-printf "%s\n" "${changed_files}" \
-    | sed 's| |\\ |g' \
-    | xargs ./node_modules/.bin/eslint --quiet
-
-[ "${?}" -ne 0 ] && exit 1
+if !(
+  printf "%s\n" "${changed_files}" |
+    sed 's| |\\ |g' |
+    xargs ./node_modules/.bin/eslint --quiet
+  )
+then
+  exit 1
+fi
 
 exit 0
--- scripts/lint-staged.sh
+++ scripts/lint-staged.sh
@@ -2,13 +2,21 @@
 
 # Adapted from: https://prettier.io/docs/en/precommit.html#option-6-shell-script
 
-staged_files="$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')"
+staged_files="$(
+  git diff --cached --name-only --diff-filter=ACMR | # get staged files
+  sed 's| |\\ |g' # escape whitespace
+)"
 
-[ -z "${staged_files}" ] && exit 0
+if [ -z "${staged_files}" ]; then
+  exit 0
+fi
 
 # Lint all selected files
 # --quiet to silence unmatched file warnings caused by .eslintignore
-if ! (printf "%s\n" "${staged_files}" | xargs ./node_modules/.bin/eslint --fix --quiet);
+if !(
+  printf "%s\n" "${staged_files}" |
+    xargs ./node_modules/.bin/eslint --fix --quiet
+  )
 then
   exit 1
 fi
