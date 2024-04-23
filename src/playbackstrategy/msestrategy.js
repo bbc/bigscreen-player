dbc5a5e62a5c504f0d028ac80ee37c5fd3aa8864
Matt Stephenson <matt.stephenson@bbc.co.uk>
2024-04-23 14:41:55 +0100

    Include initial-playback-time in DebugTool on the MSE strategy

--- src/playbackstrategy/msestrategy.js
+++ src/playbackstrategy/msestrategy.js
@@ -454,6 +454,7 @@ function MSEStrategy(mediaSources, windowType, mediaKind, playbackElement, isUHD
     if (mediaPlayer) {
       modifySource(refreshFailoverTime || failoverTime, failoverZeroPoint)
     } else {
+      DebugTool.dynamicMetric("initial-playback-time", playbackTime)
       failoverTime = playbackTime
       setUpMediaElement(playbackElement)
       setUpMediaPlayer(playbackTime)
