ab0415e11fb1771842d512c2ed81d1c4877e5c1e
Peter Clay Holden Morris-Hind <peter.morris-hind@bbc.co.uk>
2024-05-15 14:37:37 +0100

    Print Custom Player Settings on Chronicle

--- src/playbackstrategy/msestrategy.js
+++ src/playbackstrategy/msestrategy.js
@@ -14,6 +14,8 @@ import buildSourceAnchor, { TimelineZeroPoints } from "../utils/mse/build-source
 import convertTimeRangesToArray from "../utils/mse/convert-timeranges-to-array"
 
 function MSEStrategy(mediaSources, windowType, mediaKind, playbackElement, isUHD, customPlayerSettings) {
+  DebugTool.info(JSON.stringify(customPlayerSettings))
+
   let mediaPlayer
   let mediaElement
 
