1a29ed28c84eb3c836b28a7a0f981bb3e4e3a704
Ryan McCartney <33418788+ryanmccartney@users.noreply.github.com>
2024-10-30 17:01:07 +0000

    Add target-latency and current-latency to debug view

--- src/playbackstrategy/msestrategy.test.js
+++ src/playbackstrategy/msestrategy.test.js
@@ -30,6 +30,8 @@ const mockDashInstance = {
   isReady: jest.fn(),
   refreshManifest: jest.fn(),
   getDashMetrics: jest.fn(),
+  getTargetLiveDelay: jest.fn(),
+  getCurrentLiveLatency: jest.fn(),
   getDashAdapter: jest.fn(),
   getBitrateInfoListFor: jest.fn(),
   getAverageThroughput: jest.fn(),
