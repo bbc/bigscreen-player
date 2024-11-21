7036226399ab908531b2924c5838b8e313458a5a
matt-stephenson <matt.stephenson@bbc.co.uk>
2024-11-21 11:26:40 +0000

    test: native strategy tests

--- src/playbackstrategy/nativestrategy.test.js
+++ src/playbackstrategy/nativestrategy.test.js
@@ -12,6 +12,8 @@ import Playable from "./modifiers/live/playable"
 import RestartablePlayer from "./modifiers/live/restartable"
 import SeekablePlayer from "./modifiers/live/seekable"
 
+import { ManifestType } from "../models/manifesttypes"
+
 jest.mock("./legacyplayeradapter")
 
 jest.mock("./modifiers/html5")
@@ -29,7 +31,6 @@ describe("Native Strategy", () => {
   const mediaKind = "mediaKind"
   const playbackElement = "playbackElement"
   const isUHD = "isUHD"
-  const mediaSources = "mockMediaSources"
 
   const html5Player = "mockHtml5Player"
   const cehtmlPlayer = "mockCehtmlPlayer"
@@ -41,6 +42,12 @@ describe("Native Strategy", () => {
   const playablePlayer = "mockPlayablePlayer"
   const restartablePlayer = "mockRestartablePlayer"
   const seekablePlayer = "mockSeekablePlayer"
+
+  const mockMediaSources = {
+    time: jest.fn(),
+    currentSource: jest.fn().mockReturnValue(""),
+    availableSources: jest.fn().mockReturnValue([]),
+  }
 
   beforeEach(() => {
     jest.clearAllMocks()
@@ -63,133 +70,140 @@ describe("Native Strategy", () => {
     delete window.bigscreenPlayer
   })
 
-  describe("window types", () => {
-    it("calls LegacyAdapter with a static media player when called for STATIC window", () => {
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+  describe("stream types", () => {
+    it("calls LegacyAdapter with a static media player when called for STATIC stream", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.STATIC })
+
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(HTML5Player).toHaveBeenCalledWith()
 
       expect(LegacyAdapter).toHaveBeenCalled()
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, html5Player)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, html5Player)
     })
 
-    it("calls LegacyAdapter with a live media player when called for a GROWING window", () => {
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+    it("calls LegacyAdapter with a live media player when called for a DYNAMIC stream", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.DYNAMIC })
+
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(HTML5Player).toHaveBeenCalledWith()
-      expect(Playable).toHaveBeenCalledWith(html5Player, mediaSources)
+      expect(Playable).toHaveBeenCalledWith(html5Player, mockMediaSources)
 
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, playablePlayer)
-    })
-
-    it("calls LegacyAdapter with a live media player when called for a SLIDING window", () => {
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
-
-      expect(HTML5Player).toHaveBeenCalledWith()
-      expect(Playable).toHaveBeenCalledWith(html5Player, mediaSources)
-
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, playablePlayer)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, playablePlayer)
     })
   })
 
   describe("players", () => {
     it("should default to html5 when no configuration", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.STATIC })
       window.bigscreenPlayer.mediaPlayer = undefined
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(HTML5Player).toHaveBeenCalledWith()
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, html5Player)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, html5Player)
     })
 
     it("should use the cehtml when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.STATIC })
       window.bigscreenPlayer.mediaPlayer = "cehtml"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(CehtmlPlayer).toHaveBeenCalledWith()
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, cehtmlPlayer)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, cehtmlPlayer)
     })
 
     it("should use the html5 when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.STATIC })
       window.bigscreenPlayer.mediaPlayer = "html5"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(HTML5Player).toHaveBeenCalledWith()
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, html5Player)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, html5Player)
     })
 
     it("should use the samsungmaple when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.STATIC })
       window.bigscreenPlayer.mediaPlayer = "samsungmaple"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(SamsungMaplePlayer).toHaveBeenCalledWith()
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, samsungMaplePlayer)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, samsungMaplePlayer)
     })
 
     it("should use the samsungstreaming when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.STATIC })
       window.bigscreenPlayer.mediaPlayer = "samsungstreaming"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(SamsungStreamingPlayer).toHaveBeenCalledWith()
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, samsungStreamingPlayer)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, samsungStreamingPlayer)
     })
 
     it("should use the samsungstreaming2015 when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.STATIC })
       window.bigscreenPlayer.mediaPlayer = "samsungstreaming2015"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
       expect(SamsungStreaming2015Player).toHaveBeenCalledWith()
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, samsungStreaming2015Player)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, samsungStreaming2015Player)
     })
   })
 
   describe("live players", () => {
     it("should default to playable when no configuration", () => {
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.DYNAMIC })
 
-      expect(Playable).toHaveBeenCalledWith(html5Player, mediaSources)
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, playablePlayer)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
+
+      expect(Playable).toHaveBeenCalledWith(html5Player, mockMediaSources)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, playablePlayer)
     })
 
     it("should use none when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.DYNAMIC })
       window.bigscreenPlayer.liveSupport = "none"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
-      expect(None).toHaveBeenCalledWith(html5Player, mediaSources)
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, nonePlayer)
+      expect(None).toHaveBeenCalledWith(html5Player, mockMediaSources)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, nonePlayer)
     })
 
     it("should use playable when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.DYNAMIC })
       window.bigscreenPlayer.liveSupport = "playable"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
-      expect(Playable).toHaveBeenCalledWith(html5Player, mediaSources)
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, playablePlayer)
+      expect(Playable).toHaveBeenCalledWith(html5Player, mockMediaSources)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, playablePlayer)
     })
 
     it("should use restartable when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.DYNAMIC })
       window.bigscreenPlayer.liveSupport = "restartable"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
-      expect(RestartablePlayer).toHaveBeenCalledWith(html5Player, mediaSources)
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, restartablePlayer)
+      expect(RestartablePlayer).toHaveBeenCalledWith(html5Player, mockMediaSources)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, restartablePlayer)
     })
 
     it("should use seekable when configured", () => {
+      mockMediaSources.time.mockReturnValueOnce({ manifestType: ManifestType.DYNAMIC })
       window.bigscreenPlayer.liveSupport = "seekable"
 
-      NativeStrategy(mediaSources, mediaKind, playbackElement, isUHD)
+      NativeStrategy(mockMediaSources, mediaKind, playbackElement, isUHD)
 
-      expect(SeekablePlayer).toHaveBeenCalledWith(html5Player, mediaSources)
-      expect(LegacyAdapter).toHaveBeenCalledWith(mediaSources, playbackElement, isUHD, seekablePlayer)
+      expect(SeekablePlayer).toHaveBeenCalledWith(html5Player, mockMediaSources)
+      expect(LegacyAdapter).toHaveBeenCalledWith(mockMediaSources, playbackElement, isUHD, seekablePlayer)
     })
   })
 })
