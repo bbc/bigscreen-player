5cbcb6880977834437e9e970f288503a93e9e5b9
Peter Clay Holden Morris-Hind <peter.morris-hind@bbc.co.uk>
2024-11-27 11:27:05 +0000

    Always show subtitle Trim/Parse time

--- src/subtitles/imscsubtitles.js
+++ src/subtitles/imscsubtitles.js
@@ -95,17 +95,17 @@ function IMSCSubtitles(mediaPlayer, autoStart, parentElement, mediaSources, defa
             ? responseText.replace(/^.*<\?xml[^?]+\?>/i, "")
             : responseText.split(/<\?xml[^?]+\?>/i)[1] || responseText
 
-          if (isSubtitlesWhole()) {
-            DebugTool.info(`XML trim duration: ${Date.now() - preTrimTime}`)
-          }
+          // if (isSubtitlesWhole()) {
+          DebugTool.info(`XML trim duration: ${Date.now() - preTrimTime}`)
+          // }
 
           const preParseTime = Date.now()
 
           const xml = fromXML(xmlText)
 
-          if (isSubtitlesWhole()) {
-            DebugTool.info(`XML parse duration: ${Date.now() - preParseTime}`)
-          }
+          // if (isSubtitlesWhole()) {
+          DebugTool.info(`XML parse duration: ${Date.now() - preParseTime}`)
+          // }
 
           const times = xml.getMediaTimeEvents()
 
